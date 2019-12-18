11d2f06473a36e77e9c5dcd905717a3034e1c96f
Marcin Copik <mcopik@gmail.com>
2019-12-18 18:44:12 +0100

    First implementation of PAPI runner

--- cloud-frontend/local/docker/baremetal/python/runner.py
+++ cloud-frontend/local/docker/baremetal/python/runner.py
@@ -3,6 +3,46 @@ import gc, sys, imp, datetime, json, os, traceback
 RESULTS_DIR = 'results'
 LOGS_DIR = 'logs'
 
+class papi_benchmarker:
+    from pypapi import papi_low as papi
+    from pypapi import events as papi_events
+
+    def __init__(self, papi_cfg):
+        self.events = []
+        self.events_names = []
+        self.count = 0
+
+        self.papi.library_init()
+        self.events = self.papi.create_eventset()
+        for event in papi_cfg['events']:
+            self.papi.add_event(self.events, getattr(self.papi_events, event))
+        self.events_names = papi_cfg['events']
+        self.count = len(papi_cfg['events'])
+        self.results = []
+
+        self.ins_granularity = papi_cfg['overflow_instruction_granularity']
+        self.buffer_size = papi_cfg['overflow_buffer_size']
+        self.start_time = datetime.datetime.now()
+
+    def start_overflow(self):
+        self.papi.overflow_sampling(self.events, self.papi_events.PAPI_TOT_INS,
+                int(self.ins_granularity), int(self.buffer_size))
+        self.papi.start(self.events)
+
+    def stop_overflow(self):
+        self.papi.stop(self.events)
+        data = self.papi.overflow_sampling_results(self.events)
+        for vals in data:
+            for vals in zip(*[iter(vals)])*(self.count + 1):
+                measurement_time = datetime.datetime.fromtimestamp(vals[0]/1e6)
+                time = (measurement_time - self.start_time) / datetime.timedelta(microseconds = 1)
+                self.results.append([time.strftime("%s.%f"), time] + list(vals[1:]))
+
+    def finish(self):
+        self.papi.stop(self.events)
+        self.papi.cleanup_eventset(self.events)
+        self.papi.destroy_eventset(self.events)
+
 def start_benchmarking():
     gc.disable()
     return datetime.datetime.now()
@@ -16,15 +56,20 @@ def get_result_prefix(dirname, name):
     import glob
     name = os.path.join(dirname, name)
     counter = 0
-    while glob.glob( '{}-{}*'.format(name, counter) ):
+    while glob.glob( '{}_{:02d}*'.format(name, counter) ):
         counter +=1
-    return '{}-{}'.format(name, counter)
+    return '{}_{:02d}'.format(name, counter)
 
 if __name__ == "__main__":
-    cfg = json.load(open('input.json', 'r'))
+    cfg = json.load(open(sys.argv[1], 'r'))
     input_data = cfg['input']
     repetitions = cfg['benchmark']['repetitions']
     mod_name = cfg['benchmark']['module']
+    experiments = cfg['benchmark']['experiments']
+
+    run_timing = True if 'time' in experiments else False
+    run_papi = True if 'papi' in experiments else False
+
     os.system('unzip -qn code.zip')
     if os.path.exists('data.zip'):
         os.system('unzip -qn data.zip -d data')
@@ -37,18 +82,27 @@ if __name__ == "__main__":
         print(e)
         sys.exit(1)
 
-    timedata = [0] * repetitions
+    if run_timing:
+        timedata = [0] * repetitions
+    if run_papi:
+        papi_cfg = cfg['benchmark']['papi']
+        papi_experiments = papi_benchmarker(papi_cfg)
     try:
         start = start_benchmarking()
         for i in range(0, repetitions):
             begin = datetime.datetime.now()
+            if run_papi:
+                papi_experiments.start_overflow()
             res = handler(input_data)
+            if run_papi:
+                papi_experiments.stop_overflow()
             stop = datetime.datetime.now()
             print(res, file = open(
                     '{}.txt'.format(get_result_prefix(LOGS_DIR, 'output')),
                     'w'
                 ))
-            timedata[i] = [begin, stop]
+            if run_timing:
+                timedata[i] = [begin, stop]
         end = stop_benchmarking()
     except Exception as e:
         print('Exception caught!')
@@ -56,7 +110,8 @@ if __name__ == "__main__":
         traceback.print_exc()
 
     # Dump experiment data
-    experiment_data = {'system' : {}, 'date': {}, 'runtime' : {}}
+    experiment_data = {'system' : {}, 'runtime' : {}, 'experiment': {}}
+    # get currently loaded modules
     # https://stackoverflow.com/questions/4858100/how-to-list-imported-modules
     modulenames = set(sys.modules) & set(globals())
     allmodules = [sys.modules[name] for name in modulenames]
@@ -64,27 +119,43 @@ if __name__ == "__main__":
     uname = os.uname()
     for val in ['nodename', 'sysname', 'release', 'version', 'machine']:
         experiment_data['system'][val] = getattr(uname, val)
-    experiment_data['date']['start'] = start.strftime('%s.%f')
-    experiment_data['date']['end'] = end.strftime('%s.%f')
 
     experiment_data['runtime']['version'] = platform.python_version()
     experiment_data['runtime']['modules'] = str(allmodules)
-
     experiment_data['input'] = cfg
 
-    result = get_result_prefix(RESULTS_DIR, 'time')
-    with open('{}.json'.format(result), 'w') as f:
-        json.dump(experiment_data, f, indent = 2)
+    experiment_data['experiment']['repetitions'] = repetitions
+    experiment_data['experiment']['start'] = start.strftime('%s.%f')
+    experiment_data['experiment']['end'] = end.strftime('%s.%f')
+
 
     # Dump results
-    with open('{}.csv'.format(result), 'w') as f:
-        csv_writer = csv.writer(f)
-        csv_writer.writerow(['Begin','End','Duration'])
-        for i in range(0, len(timedata)):
-            csv_writer.writerow([
-                    timedata[i][0].strftime('%s.%f'),
-                    timedata[i][1].strftime('%s.%f'),
-                    (timedata[i][1] - timedata[i][0]) /
-                        datetime.timedelta(microseconds=1)
-                ])
+    if run_timing:
+        result = get_result_prefix(RESULTS_DIR, 'time')
+        with open('{}.json'.format(result), 'w') as f:
+            json.dump(experiment_data, f, indent = 2)
+
+        with open('{}.csv'.format(result), 'w') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(['Begin','End','Duration'])
+            for i in range(0, len(timedata)):
+                csv_writer.writerow([
+                        timedata[i][0].strftime('%s.%f'),
+                        timedata[i][1].strftime('%s.%f'),
+                        (timedata[i][1] - timedata[i][0]) /
+                            datetime.timedelta(microseconds=1)
+                    ])
+    if run_papi:
+        papi_experiments.finish()
+        result = get_result_prefix(RESULTS_DIR, 'papi')
+        with open('{}.json'.format(result), 'w') as f:
+            json.dump(experiment_data, f, indent = 2)
+
+        with open('{}.csv'.format(result), 'w') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(
+                    ['Time','RelativeTime'] + papi_experiments.events_names
+                )
+            for val in papi_experiments.result:
+                csv_writer.writerow(val)
 
