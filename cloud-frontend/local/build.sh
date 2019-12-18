34707d5b3c74112dc18854f506a47afe63f87889
Marcin Copik <mcopik@gmail.com>
2019-12-18 01:12:14 +0100

    Correctly add pypapi to docker

--- cloud-frontend/local/build.sh
+++ cloud-frontend/local/build.sh
@@ -1,4 +1,7 @@
 
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Baremetal servers
+cp -R $(pwd)/sebs-build/install/pypapi "${DIR}/docker/baremetal/python/"
 docker build -f "${DIR}/docker/baremetal/python/Dockerfile" -t sebs-local-python "${DIR}/docker/baremetal/python"
+rm -rf "${DIR}/docker/baremetal/python/pypapi"
