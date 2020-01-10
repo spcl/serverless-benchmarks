fd64b7810266e4416bdee3da3008bddcac53356f
Marcin Copik <mcopik@gmail.com>
2020-01-10 18:51:16 +0100

    NodeJS handler

---
+++ cloud-frontend/aws/nodejs/handler.js
@@ -0,0 +1,17 @@
+
+var func = require('./function')
+
+exports.handler = async function(event, context) {
+  var start = process.hrtime();
+  var ret = func.handler(event);
+  return ret.then(
+    (result) => {
+      var elapsed = process.hrtime(start);
+      var micro = elapsed[1] / 1e3 + elapsed[0] * 1e6;
+      return {time: micro, message: result};
+    },
+    (error) => {
+      throw(error);
+    }
+  );
+}
