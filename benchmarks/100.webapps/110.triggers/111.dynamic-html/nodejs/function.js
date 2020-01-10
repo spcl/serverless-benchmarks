d45eb89d4d6af14fec57fb3196200f1a9531500a
Marcin Copik <mcopik@gmail.com>
2020-01-10 17:49:01 +0100

    Add missing output

--- benchmarks/100.webapps/110.triggers/111.dynamic-html/nodejs/function.js
+++ benchmarks/100.webapps/110.triggers/111.dynamic-html/nodejs/function.js
@@ -7,23 +7,18 @@ function random(b, e) {
 }
 
 exports.handler = async function(event, context) {
-	random_numbers = new Array(event.random_len)
+  var random_numbers = new Array(event.random_len);
   for(var i = 0; i < event.random_len; ++i) {
-    random_numbers[i] = random(0, 100)
+    random_numbers[i] = random(0, 100);
   }
-	var input = {
-		cur_time: new Date().toLocaleString(),
-		username: event.username,
-		random_numbers: random_numbers
-	};
+  var input = {
+    cur_time: new Date().toLocaleString(),
+    username: event.username,
+    random_numbers: random_numbers
+  };
 
-	var file = path.join('templates', 'template.html');
-	return fs.readFile(file, "utf-8",
-		function(err, data) {
-			if(err) throw(err);	
-			var output = Mustache.render(data, input);
-			console.log(output)
-			return output;
-		}
-	);	
-}
+  var file = path.join('templates', 'template.html');
+  var data = fs.readFileSync(file, "utf-8");
+  var output = Mustache.render(data, input);
+  return output;
+};
