12937c4360cf143ff796230b9475be2b198cacb6
Marcin Copik <mcopik@gmail.com>
2020-01-11 01:13:38 +0100

    Move common files across runtimes

--- cloud-frontend/local/build.sh
+++ cloud-frontend/local/build.sh
@@ -2,8 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Baremetal servers
-cp -R $(pwd)/sebs-build/install/pypapi "${DIR}/python/"
-docker build -f Dockerfile.python -t sebs-local-python "${DIR}/python"
-rm -rf "${DIR}/python/pypapi"
+cp -R $(pwd)/sebs-build/install/pypapi "${DIR}"
+docker build -f Dockerfile.python -t sebs-local-python "${DIR}"
+rm -rf "${DIR}/pypapi"
 
-docker build -f Dockerfile.nodejs -t sebs-local-nodejs "${DIR}/nodejs"
+docker build -f Dockerfile.nodejs -t sebs-local-nodejs "${DIR}"
