356bb12e859d1a5fe8b65951d341019544d90663
Marcin Copik <mcopik@gmail.com>
2020-01-12 01:56:50 +0100

    NodeJS warm time measurements

--- cloud-frontend/local/nodejs/timeit.sh
+++ cloud-frontend/local/nodejs/timeit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ts=$(date +%s%N);
-node -e "var fs = require('fs'), f = require('./function');
+node --expose-gc -e "var fs = require('fs'), f = require('./function');
 async function test() {
   var input = JSON.parse(fs.readFileSync('input.json', 'utf-8'));
   return await f.handler(input);
--- cloud-frontend/local/nodejs/tools.js
+++ cloud-frontend/local/nodejs/tools.js
@@ -1,4 +1,13 @@
 
+const glob = require('glob'),
+      path = require('path');
+
+const RESULTS_DIR = 'results';
+exports.RESULTS_DIR = RESULTS_DIR;
+const LOGS_DIR = 'logs';
+exports.LOGS_DIR = LOGS_DIR;
+
+
 exports.get_config = function () {
   return {
     name: 'nodejs',
@@ -6,3 +15,30 @@ exports.get_config = function () {
     modules: process.moduleLoadList
   };
 }
+
+exports.start_benchmarking = function() {
+  return Date.now()
+}
+
+exports.stop_benchmarking = function() {
+  return Date.now()
+}
+
+exports.get_result_prefix = function(dirname, name, suffix) {
+  name = path.join(dirname, name);
+  let counter = 0
+  while(
+    glob.sync(
+      name + '_' + counter.toString().padStart(2, '0') + '*.' + suffix
+    ).length
+  ) {
+    counter += 1
+  }
+  // util.format ignores padding zeroes
+  return name + '_' + counter.toString().padStart(2, '0') + '.' + suffix
+}
+
+exports.process_timestamps = function(timestamps) {
+
+}
+
