029cd920900e56b2fcfa840dce823ec861ebd838
Marcin Copik <mcopik@gmail.com>
2020-01-14 01:08:44 +0100

    [210.thumbnailer] return value from function

--- benchmarks/200.multimedia/210.thumbnailer/nodejs/function.js
+++ benchmarks/200.multimedia/210.thumbnailer/nodejs/function.js
@@ -7,15 +7,16 @@ let storage_handler = new storage.storage();
 exports.handler = async function(event) {
   input_bucket = event.bucket.input
   output_bucket = event.bucket.output
-  key = event.object.key
+  let key = event.object.key
   width = event.object.width
   height = event.object.height
   let pos = key.lastIndexOf('.');
-  let key = key.substr(0, pos < 0 ? key.length : pos) + '.png';
+  let upload_key = key.substr(0, pos < 0 ? key.length : pos) + '.png';
 
   const sharp_resizer = sharp().resize(width, height).png();
   let input_data = storage_handler.downloadStream(input_bucket, key);
-  let [writeStream, promise] = storage_handler.uploadStream(output_bucket, key);
+  let [writeStream, promise] = storage_handler.uploadStream(output_bucket, upload_key);
   input_data.pipe(sharp_resizer).pipe(writeStream);
   await promise;
+  return {bucket: output_bucket, key: upload_key}
 };
