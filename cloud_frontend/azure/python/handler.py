659cf9c620c1f1c6a5de33e815e6791262b9ba2d
Marcin Copik <mcopik@gmail.com>
2020-01-15 18:41:50 +0100

    [azure] syntax fixes for azure handler

--- cloud_frontend/azure/python/handler.py
+++ cloud_frontend/azure/python/handler.py
@@ -1,15 +1,15 @@
 
-import datetime
+import datetime, json
 
 import azure.functions as func
 
-import function
+from . import function
 
 # TODO: usual trigger
 # implement support for blob and others
-def handler(req: func.HttpRequest) -> func.HttpResponse
+def main(req: func.HttpRequest) -> func.HttpResponse:
     begin = datetime.datetime.now()
-    ret = function.handler(req.params)
+    ret = function.handler(req.get_json())
     end = datetime.datetime.now()
     return func.HttpResponse(
         json.dumps({
