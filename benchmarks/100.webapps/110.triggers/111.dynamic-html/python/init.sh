91ec2808283f0547f5106199ddba2a738d4a06a2
Marcin Copik <mcopik@gmail.com>
2020-01-15 15:21:32 +0100

    [111.dynamic-html] remove zipping from benchmark packing of templates

--- benchmarks/100.webapps/110.triggers/111.dynamic-html/python/init.sh
+++ benchmarks/100.webapps/110.triggers/111.dynamic-html/python/init.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-ZIP_FILE=$1
+DIR=$1
 VERBOSE=$2
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 path="${SCRIPT_DIR}/templates/"
 if [ "$VERBOSE" = true ]; then
-  echo "Update ${ZIP_FILE} with static templates ${path}"
+  echo "Update ${DIR} with static templates ${path}"
 fi
-pushd ${SCRIPT_DIR} > /dev/null
-zip -qu ${ZIP_FILE} templates/*
-popd > /dev/null
+cp -r ${SCRIPT_DIR}/templates ${DIR}
