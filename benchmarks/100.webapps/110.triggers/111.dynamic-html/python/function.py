5890fa4c1a87854d373395015922afd15991e0f9
Marcin Copik <mcopik@gmail.com>
2020-01-15 18:41:32 +0100

    [111.dynamic-html] ensures it works with every directory location

--- benchmarks/100.webapps/110.triggers/111.dynamic-html/python/function.py
+++ benchmarks/100.webapps/110.triggers/111.dynamic-html/python/function.py
@@ -1,9 +1,11 @@
-from time import time                                                           
 from datetime import datetime                                                   
 from random import sample  
+from os import path
+from time import time                                                           
 
 from jinja2 import Template
 
+SCRIPT_DIR = path.abspath(path.join(path.dirname(__file__)))
 
 def handler(event):
 
@@ -12,7 +14,7 @@ def handler(event):
     size = event.get('random_len')
     cur_time = datetime.now()
     random_numbers = sample(range(0, 100), size)
-    template = Template( open('templates/template.html', 'r').read())
+    template = Template( open(path.join(SCRIPT_DIR, 'templates', 'template.html'), 'r').read())
     html = template.render(username = name, cur_time = cur_time, random_numbers = random_numbers)
     # end timing
     # dump stats 
