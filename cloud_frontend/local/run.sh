88125250ce9b58f8a0ba17fc467adbdfb8aad8db
Marcin Copik <mcopik@gmail.com>
2020-01-18 14:43:00 +0100

    [local] Infinite loop in incorrect log name detection

--- cloud_frontend/local/run.sh
+++ cloud_frontend/local/run.sh
@@ -5,7 +5,8 @@ file_name=logs/execution_00.log
 counter=0
 
 while [ -e "${file_name}" ]; do
-  file_name=$(printf '%s_%02d.log' "logs/execution" "$(( ++counter ))")
+  counter=$((counter + 1))
+  file_name=$(printf '%s_%02d.log' "logs/execution" "$(( counter ))")
 done
 
 script -c "python3 runner.py ${EXPERIMENT_INPUT}" -f "${file_name}"
