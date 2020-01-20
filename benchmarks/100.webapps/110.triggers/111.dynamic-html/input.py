870f30a7de1258cf0cb6581d5b12d1f348ba7381
Marcin Copik <mcopik@gmail.com>
2020-01-20 22:57:08 +0100

    [111.dynamic-html] Fix API

--- benchmarks/100.webapps/110.triggers/111.dynamic-html/input.py
+++ benchmarks/100.webapps/110.triggers/111.dynamic-html/input.py
@@ -8,7 +8,7 @@ size_generators = {
 def buckets_count():
     return (0, 0)
 
-def generate_input(size, input_buckets, output_buckets, upload_func):
+def generate_input(data_dir, size, input_buckets, output_buckets, upload_func):
     input_config = {'username': 'testname'} 
     input_config['random_len'] = size_generators[size]
     return input_config
