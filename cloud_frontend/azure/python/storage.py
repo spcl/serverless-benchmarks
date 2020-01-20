4e2bd5b919d0274845c15664966a2c00ac76be4f
Marcin Copik <mcopik@gmail.com>
2020-01-20 20:29:02 +0100

    [azure] Extend storage API

--- cloud_frontend/azure/python/storage.py
+++ cloud_frontend/azure/python/storage.py
@@ -12,6 +12,23 @@ class storage:
         self.client = BlobServiceClient.from_connection_string(
                 os.getenv('STORAGE_CONNECTION_STRING')
             )
+
+    def upload(self, container, file, filepath):
+        with open(filepath, 'rb') as data:
+            upload_stream(container, file, data)
+
+    def download(self, container, file, filepath):
+        with open(filepath, 'wb') as download_file:
+            download_file.write( self.download_stream(container, file) )
+    
+    def download_directory(self, container, prefix, path):
+        client = self.client.get_container_client(container=container)
+        objects = client.list_blobs(name_starts_with=prefix)
+        for obj in objects:
+            file_name = obj.name
+            path_to_file = os.path.dirname(file_name)
+            os.makedirs(os.path.join(path, path_to_file), exist_ok=True)
+            self.download(container, file_name, os.path.join(path, file_name))
     
     # it seems that JS does not have an API that would allow to 
     # upload/download data without going through container client
