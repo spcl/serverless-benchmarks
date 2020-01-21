000fa51d6892d1b6eaa1c9ca9ac4dd17372bc5a6
Marcin Copik <mcopik@gmail.com>
2020-01-21 19:15:10 +0100

    [system] fix Azure package name

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 env_dir=sebs-virtualenv
 if [ ! -d ${env_dir} ]; then
@@ -10,33 +10,31 @@ if [ ! -d ${env_dir} ]; then
   # aws
   pip3 install boto3
   # azure
-  pip3 install azure-blob-storage
-  #
-
+  pip3 install azure-storage-blob
 fi
 
-# node
-env_dir=sebs-nodeenv
-if [ ! -d ${env_dir} ]; then
-  nodeenv ${env_dir}
-  source ${env_dir}/bin/activate
-fi
-
-git submodule update --init --recursive
-
-build_dir=sebs-build
-mkdir -p ${build_dir}
-mkdir -p ${build_dir}/install
-install_dir=$(pwd)/${build_dir}/install
-
-pushd third-party/pypapi > /dev/null && git checkout low_api_overflow
-pip3 install -r requirements.txt
-python3 setup.py build && python3 pypapi/papi_build.py
-popd > /dev/null
-
-mkdir -p ${install_dir}/pypapi
-cd third-party/pypapi
-cp -R pypapi/*.py ${install_dir}/pypapi
-cp -R pypapi/*.so ${install_dir}/pypapi
-cd ..
+## node
+#env_dir=sebs-nodeenv
+#if [ ! -d ${env_dir} ]; then
+#  nodeenv ${env_dir}
+#  source ${env_dir}/bin/activate
+#fi
+#
+#git submodule update --init --recursive
+#
+#build_dir=sebs-build
+#mkdir -p ${build_dir}
+#mkdir -p ${build_dir}/install
+#install_dir=$(pwd)/${build_dir}/install
+#
+#pushd third-party/pypapi > /dev/null && git checkout low_api_overflow
+#pip3 install -r requirements.txt
+#python3 setup.py build && python3 pypapi/papi_build.py
+#popd > /dev/null
+#
+#mkdir -p ${install_dir}/pypapi
+#cd third-party/pypapi
+#cp -R pypapi/*.py ${install_dir}/pypapi
+#cp -R pypapi/*.so ${install_dir}/pypapi
+#cd ..
 
