aa1afc73901af714fdd222dec0a3b6378e48457d
Marcin Copik <mcopik@gmail.com>
2020-01-28 19:09:27 +0100

    [120.uploader] Add benchmark

---
+++ benchmarks/100.webapps/120.uploader/input.py
@@ -0,0 +1,18 @@
+
+url_generators = {
+    # source: mlperf fake_imagenet.sh. 230 kB
+    'test' : 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Jammlich_crop.jpg/800px-Jammlich_crop.jpg',
+    # video: HPX source code, 6.7 MB
+    'small': 'https://github.com/STEllAR-GROUP/hpx/archive/1.4.0.zip',
+    # resnet model from pytorch. 98M
+    'large':  'https://download.pytorch.org/models/resnet50-19c8e357.pth'
+}
+
+def buckets_count():
+    return (0, 1)
+
+def generate_input(data_dir, size, input_buckets, output_buckets, upload_func):
+    input_config = {'object': {}, 'bucket': {}}
+    input_config['object']['url'] = url_generators[size]
+    input_config['bucket']['output'] = output_buckets[0]
+    return input_config
---
+++ benchmarks/100.webapps/120.uploader/python/function.py
@@ -0,0 +1,42 @@
+
+import datetime
+import os
+import uuid
+
+import urllib.request
+
+from . import storage
+client = storage.storage.get_instance()
+
+
+def handler(event):
+  
+    output_bucket = event.get('bucket').get('output')
+    url = event.get('object').get('url')
+    name = os.path.basename(url)
+    download_path = '/tmp/{}'.format(name)
+
+    process_begin = datetime.datetime.now()
+    urllib.request.urlretrieve(url, filename=download_path)
+    size = os.path.getsize(download_path)
+    process_end = datetime.datetime.now()
+
+    upload_begin = datetime.datetime.now()
+    client.upload(output_bucket, name, download_path)
+    upload_end = datetime.datetime.now()
+
+    process_time = (process_end - process_begin) / datetime.timedelta(microseconds=1)
+    upload_time = (upload_end - upload_begin) / datetime.timedelta(microseconds=1)
+    return {
+            'result': {
+                'bucket': output_bucket,
+                'url': url
+            },
+            'measurement': {
+                'download_time': 0,
+                'download_size': 0,
+                'upload_time': upload_time,
+                'upload_size': size,
+                'compute_time': process_time
+            }
+    }
