cf00d6d3de2262f778d214899c62811a438214df
Marcin Copik <mcopik@gmail.com>
2020-01-29 14:05:17 +0100

    Update install file

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [ ! -d ${env_dir} ]; then
   python3 -mvenv ${env_dir}
   source ${env_dir}/bin/activate
   echo 'Install docker package for python'
-  pip3 install docker wheel bottle waitress minio nodeenv requests
+  pip3 install docker wheel bottle waitress minio nodeenv requests py-cpuinfo
   # aws
   pip3 install boto3
   # azure
@@ -20,18 +20,18 @@ fi
 #  source ${env_dir}/bin/activate
 #fi
 #
-#git submodule update --init --recursive
+git submodule update --init --recursive
 #
 #build_dir=sebs-build
 #mkdir -p ${build_dir}
 #mkdir -p ${build_dir}/install
 #install_dir=$(pwd)/${build_dir}/install
-#
-#pushd third-party/pypapi > /dev/null && git checkout low_api_overflow
-#pip3 install -r requirements.txt
-#python3 setup.py build && python3 pypapi/papi_build.py
-#popd > /dev/null
-#
+
+pushd third-party/pypapi > /dev/null && git checkout low_api_overflow
+pip3 install -r requirements.txt
+python3 setup.py build && python3 pypapi/papi_build.py
+popd > /dev/null
+
 #mkdir -p ${install_dir}/pypapi
 #cd third-party/pypapi
 #cp -R pypapi/*.py ${install_dir}/pypapi
