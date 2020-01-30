4b33c05d450173c2aee9b0bafc7ec635d5ee5eea
Marcin Copik <mcopik@gmail.com>
2020-01-30 19:02:43 +0100

    [aws] Return cold start information

--- cloud_frontend/aws/python/handler.py
+++ cloud_frontend/aws/python/handler.py
@@ -4,10 +4,14 @@ import datetime, io, json, os, sys
 # Add current directory to allow location of packages
 sys.path.append(os.path.join(os.path.dirname(__file__), '.python_packages/lib/site-packages'))
 
-
 # TODO: usual trigger
 # implement support for S3 and others
 def handler(event, context):
+
+    # HTTP trigger with API Gateaway
+    if 'httpMethod' in event:
+        print(event['body'])
+        event = json.loads(event['body'])
     begin = datetime.datetime.now()
     from function import function
     ret = function.handler(event)
@@ -32,9 +36,21 @@ def handler(event, context):
     else:
         results_time = 0
 
+    # cold test
+    is_cold = False
+    fname = os.path.join('/tmp','cold_run')
+    if not os.path.exists(fname):
+        is_cold = True
+        open(fname, 'a').close()
+
     return {
-        'compute_time': (end - begin) / datetime.timedelta(microseconds=1),
-        'results_time': results_time,
-        'result': log_data
+        'statusCode': 200,
+        'body': json.dumps({
+            'begin': begin.strftime('%s.%f'),
+            'end': end.strftime('%s.%f'),
+            'results_time': results_time,
+            'is_cold': is_cold,
+            'result': log_data
+        })
     }
 
