bbf6d60404d7fb5e0ad0aea1c416413ed3a787ab
Marcin Copik <mcopik@gmail.com>
2020-05-19 23:02:54 +0200

    Format code

--- sebs/cache.py
+++ sebs/cache.py
@@ -1,12 +1,11 @@
-
+# https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
+import collections.abc
 import datetime
 import json
 import logging
 import os
 import shutil
 
-# https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
-import collections.abc
 
 def update(d, u):
     for k, v in u.items():
@@ -16,12 +15,14 @@ def update(d, u):
             d[k] = v
     return d
 
+
 def update_dict(cfg, val, keys):
     def map_keys(obj, val, keys):
         if len(keys):
-            return { keys[0]: map_keys(obj, val, keys[1:]) }
+            return {keys[0]: map_keys(obj, val, keys[1:])}
         else:
             return val
+
     update(cfg, map_keys(cfg, val, keys))
 
 
@@ -29,10 +30,10 @@ class Cache:
 
     cache_dir = None
     cached_config = {}
-    '''
+    """
         Indicate that cloud offerings updated credentials or settings.
         Thus we have to write down changes.
-    '''
+    """
     config_updated = False
 
     def __init__(self, cache_dir):
@@ -43,35 +44,36 @@ class Cache:
             self.load_config()
 
     def load_config(self):
-        for cloud in ['azure', 'aws']:
-            cloud_config_file = os.path.join(self.cache_dir, '{}.json'.format(cloud))
+        for cloud in ["azure", "aws"]:
+            cloud_config_file = os.path.join(self.cache_dir, "{}.json".format(cloud))
             if os.path.exists(cloud_config_file):
-                self.cached_config[cloud] = json.load(open(cloud_config_file, 'r'))
+                self.cached_config[cloud] = json.load(open(cloud_config_file, "r"))
 
     def get_config(self, cloud):
         return self.cached_config[cloud] if cloud in self.cached_config else None
 
-    '''
+    """
         Update config values. Sets flag to save updated content in the end.
         val: new value to store
         keys: array of consecutive keys for multi-level dictionary
-    '''
+    """
+
     def update_config(self, val, keys):
         update_dict(self.cached_config, val, keys)
         self.config_updated = True
 
     def shutdown(self):
         if self.config_updated:
-            for cloud in ['azure', 'aws']:
+            for cloud in ["azure", "aws"]:
                 if cloud in self.cached_config:
                     cloud_config_file = os.path.join(
-                            self.cache_dir, '{}.json'.format(cloud)
-                        )
-                    logging.info('Update cached config {}'.format(cloud_config_file))
-                    with open(cloud_config_file, 'w') as out:
+                        self.cache_dir, "{}.json".format(cloud)
+                    )
+                    logging.info("Update cached config {}".format(cloud_config_file))
+                    with open(cloud_config_file, "w") as out:
                         json.dump(self.cached_config[cloud], out, indent=2)
 
-    '''
+    """
         Acccess cached config of a benchmark.
 
         :param deployment: allowed deployment clouds or local
@@ -79,15 +81,16 @@ class Cache:
         :param  language:
 
         :return: a JSON config or None when not exists
-    '''
-    def get_benchmark_config(self, deployment :str, benchmark :str):
+    """
+
+    def get_benchmark_config(self, deployment: str, benchmark: str):
         benchmark_dir = os.path.join(self.cache_dir, benchmark)
         if os.path.exists(benchmark_dir):
-            with open(os.path.join(benchmark_dir, 'config.json'), 'r') as fp:
+            with open(os.path.join(benchmark_dir, "config.json"), "r") as fp:
                 cfg = json.load(fp)
                 return cfg[deployment] if deployment in cfg else None
 
-    '''
+    """
         Acccess cached version of a function.
 
         :param deployment: allowed deployment clouds or local
@@ -95,38 +98,37 @@ class Cache:
         :param  language:
 
         :return: a tuple of JSON config and absolute path to code or None
-    '''
-    def get_function(self, deployment :str, benchmark :str, language :str):
+    """
+
+    def get_function(self, deployment: str, benchmark: str, language: str):
         cfg = self.get_benchmark_config(deployment, benchmark)
         if cfg and language in cfg:
-            return (
-                    cfg[language],
-                    os.path.join(self.cache_dir, cfg[language]['code'])
-                )
+            return (cfg[language], os.path.join(self.cache_dir, cfg[language]["code"]))
         else:
             return (None, None)
 
-    '''
+    """
         Acccess cached storage config of a benchmark.
 
         :param deployment: allowed deployment clouds or local
         :param benchmark:
 
         :return: a JSON config or None
-    '''
-    def get_storage_config(self, deployment :str, benchmark :str):
-        cfg = self.get_benchmark_config(deployment, benchmark)
-        return cfg['storage'] if cfg else None
+    """
 
-    def update_storage(self, deployment :str, benchmark :str, config :dict):
+    def get_storage_config(self, deployment: str, benchmark: str):
+        cfg = self.get_benchmark_config(deployment, benchmark)
+        return cfg["storage"] if cfg else None
+
+    def update_storage(self, deployment: str, benchmark: str, config: dict):
         benchmark_dir = os.path.join(self.cache_dir, benchmark)
-        with open(os.path.join(benchmark_dir, 'config.json'), 'r') as fp:
+        with open(os.path.join(benchmark_dir, "config.json"), "r") as fp:
             cached_config = json.load(fp)
-        cached_config[deployment]['storage'] = config
-        with open(os.path.join(benchmark_dir, 'config.json'), 'w') as fp:
+        cached_config[deployment]["storage"] = config
+        with open(os.path.join(benchmark_dir, "config.json"), "w") as fp:
             json.dump(cached_config, fp, indent=2)
 
-    '''
+    """
         Update stored function code and configuration.
         Replaces entire config for specified deployment
 
@@ -135,31 +137,37 @@ class Cache:
         :param language:
         :param code_package:
         :param config: Updated config values to use.
-    '''
-    def update_function(self, deployment :str, benchmark :str, language :str,
-            code_package :str, config :dict):
+    """
+
+    def update_function(
+        self,
+        deployment: str,
+        benchmark: str,
+        language: str,
+        code_package: str,
+        config: dict,
+    ):
 
         benchmark_dir = os.path.join(self.cache_dir, benchmark)
         cached_dir = os.path.join(benchmark_dir, deployment, language)
         # copy code
         if os.path.isdir(code_package):
-            dest = os.path.join(cached_dir, 'code')
+            dest = os.path.join(cached_dir, "code")
             shutil.rmtree(dest)
             shutil.copytree(code_package, dest)
         # copy zip file
         else:
             shutil.copy2(code_package, cached_dir)
         # update JSON config
-        with open(os.path.join(benchmark_dir, 'config.json'), 'r') as fp:
+        with open(os.path.join(benchmark_dir, "config.json"), "r") as fp:
             cached_config = json.load(fp)
         date = str(datetime.datetime.now())
         cached_config[deployment][language] = config
-        cached_config[deployment][language]['date']['modified'] = date
-        with open(os.path.join(benchmark_dir, 'config.json'), 'w') as fp:
+        cached_config[deployment][language]["date"]["modified"] = date
+        with open(os.path.join(benchmark_dir, "config.json"), "w") as fp:
             json.dump(cached_config, fp, indent=2)
 
-
-    '''
+    """
         Add new function to cache.
 
         :param deployment:
@@ -168,9 +176,17 @@ class Cache:
         :param code_package: Path to directory/ZIP with code.
         :param language_config: Configuration of language and code.
         :param storage_config: Configuration of storage buckets.
-    '''
-    def add_function(self, deployment, benchmark, language, code_package,
-            language_config, storage_config):
+    """
+
+    def add_function(
+        self,
+        deployment,
+        benchmark,
+        language,
+        code_package,
+        language_config,
+        storage_config,
+    ):
 
         benchmark_dir = os.path.join(self.cache_dir, benchmark)
         os.makedirs(benchmark_dir, exist_ok=True)
@@ -182,7 +198,7 @@ class Cache:
 
             # copy code
             if os.path.isdir(code_package):
-                cached_location = os.path.join(cached_dir, 'code')
+                cached_location = os.path.join(cached_dir, "code")
                 shutil.copytree(code_package, cached_location)
             # copy zip file
             else:
@@ -191,36 +207,33 @@ class Cache:
                 shutil.copy2(code_package, cached_dir)
 
             config = {
-                deployment: {
-                    language: language_config,
-                    'storage': storage_config
-                }
+                deployment: {language: language_config, "storage": storage_config}
             }
 
             # don't store absolute path to avoid problems with moving cache dir
             relative_cached_loc = os.path.relpath(cached_location, self.cache_dir)
-            config[deployment][language]['code'] = relative_cached_loc
+            config[deployment][language]["code"] = relative_cached_loc
             date = str(datetime.datetime.now())
-            config[deployment][language]['date'] = {'created': date, 'modified': date}
+            config[deployment][language]["date"] = {"created": date, "modified": date}
             # make sure to not replace other entries
-            if os.path.exists(os.path.join(benchmark_dir, 'config.json')):
-                with open(os.path.join(benchmark_dir, 'config.json'), 'r') as fp:
+            if os.path.exists(os.path.join(benchmark_dir, "config.json")):
+                with open(os.path.join(benchmark_dir, "config.json"), "r") as fp:
                     cached_config = json.load(fp)
                     if deployment in cached_config:
                         cached_config[deployment][language] = language_config
                     else:
                         cached_config[deployment] = {
                             language: language_config,
-                            'storage': storage_config
+                            "storage": storage_config,
                         }
                     config = cached_config
-            with open(os.path.join(benchmark_dir, 'config.json'), 'w') as fp:
+            with open(os.path.join(benchmark_dir, "config.json"), "w") as fp:
                 json.dump(config, fp, indent=2)
 
         else:
             # TODO: update
             raise RuntimeError(
-                    'Cached application {} for {} already exists!'.format(
-                        benchmark, deployment
-                    )
+                "Cached application {} for {} already exists!".format(
+                    benchmark, deployment
                 )
+            )
--- sebs/code_package.py
+++ sebs/code_package.py
@@ -1,5 +1,3 @@
-
-import docker
 import glob
 import hashlib
 import json
@@ -8,10 +6,13 @@ import os
 import shutil
 import subprocess
 
-from .cache import Cache
-from .utils import project_absolute_path, find_benchmark
+import docker
 
-'''
+from .cache import Cache
+from .utils import find_benchmark, project_absolute_path
+
+
+"""
     Creates code package representing a benchmark with all code and assets
     prepared and dependency install performed within Docker image corresponding
     to the cloud deployment.
@@ -21,13 +22,14 @@ from .utils import project_absolute_path, find_benchmark
     2)  Otherwise, the hash of the entire benchmark is computed and compared
         with the cached value. If changed, then rebuilt then benchmark.
     3)  Otherwise, just return the path to cache code.
-'''
-class CodePackage:
+"""
 
+
+class CodePackage:
     @property
     def benchmark(self):
         return self._benchmark
-    
+
     @property
     def benchmark_path(self):
         return self._benchmark_path
@@ -56,33 +58,41 @@ class CodePackage:
     def code_size(self):
         return self._code_size
 
-    @property
+    @property  # noqa: A003
     def hash(self):
         if not self._hash_value:
             path = os.path.join(self.benchmark_path, self._language)
             self._hash_value = CodePackage.hash_directory(path, self._language)
         return self._hash_value
 
-    def __init__(self, benchmark: str, config: dict, output_dir: str,
-            system_config: dict, cache_client: Cache,
-            docker_client: docker.client,
-            forced_update: bool=False):
+    def __init__(
+        self,
+        benchmark: str,
+        config: dict,
+        output_dir: str,
+        system_config: dict,
+        cache_client: Cache,
+        docker_client: docker.client,
+        forced_update: bool = False,
+    ):
         self._benchmark = benchmark
-        self._deployment = config['experiments']['deployment']['name']
-        self._language = config['experiments']['language']
-        self._runtime = config['experiments']['runtime']
-        self._benchmark_path = find_benchmark(self.benchmark, 'benchmarks')
+        self._deployment = config["experiments"]["deployment"]["name"]
+        self._language = config["experiments"]["language"]
+        self._runtime = config["experiments"]["runtime"]
+        self._benchmark_path = find_benchmark(self.benchmark, "benchmarks")
         if not self._benchmark_path:
-            raise RuntimeError('Benchmark {benchmark} not found!'.format(benchmark=self._benchmark))
-        with open( os.path.join(self.benchmark_path, 'config.json') ) as json_file:
-            self._benchmark_config = json.load(json_file)
-        if self._language not in self._benchmark_config['languages']:
             raise RuntimeError(
-                    'Benchmark {} not available for language {}'.format(
-                        self.benchmark, self._language
-                    )
+                "Benchmark {benchmark} not found!".format(benchmark=self._benchmark)
+            )
+        with open(os.path.join(self.benchmark_path, "config.json")) as json_file:
+            self._benchmark_config = json.load(json_file)
+        if self._language not in self._benchmark_config["languages"]:
+            raise RuntimeError(
+                "Benchmark {} not available for language {}".format(
+                    self.benchmark, self._language
                 )
-        self._system_config = system_config['languages'][self._language]
+            )
+        self._system_config = system_config["languages"][self._language]
         self._cache_client = cache_client
         self._docker_client = docker_client
         self._hash_value = None
@@ -94,38 +104,38 @@ class CodePackage:
         if not self.is_cached or not self.is_cached_valid:
             self._code_location = self.build(output_dir)
 
-    '''
+    """
         Compute MD5 hash of an entire directory.
-    '''
+    """
+
     @staticmethod
     def hash_directory(directory: str, language: str):
 
         hash_sum = hashlib.md5()
         FILES = {
-            'python': ['*.py', 'requirements.txt*'],
-            'nodejs': ['*.js', 'package.json']
+            "python": ["*.py", "requirements.txt*"],
+            "nodejs": ["*.js", "package.json"],
         }
-        NON_LANG_FILES = ['*.sh', '*.json']
+        NON_LANG_FILES = ["*.sh", "*.json"]
         selected_files = FILES[language] + NON_LANG_FILES
         for file_type in selected_files:
-            for f in glob.glob( os.path.join(directory, file_type) ):
+            for f in glob.glob(os.path.join(directory, file_type)):
                 path = os.path.join(directory, f)
-                with open(path, 'rb') as opened_file:
-                    hash_sum.update( opened_file.read() )
+                with open(path, "rb") as opened_file:
+                    hash_sum.update(opened_file.read())
         return hash_sum.hexdigest()
-
 
     def query_cache(self):
         self._cached_config, self._code_location = self._cache_client.get_function(
-                    deployment=self._deployment,
-                    benchmark=self._benchmark,
-                    language=self._language
+            deployment=self._deployment,
+            benchmark=self._benchmark,
+            language=self._language,
         )
         if self.cached_config is not None:
             # compare hashes
             current_hash = self.hash
-            old_hash = self.cached_config['hash']
-            self._code_size = self.cached_config['code_size']
+            old_hash = self.cached_config["hash"]
+            self._code_size = self.cached_config["code_size"]
             self._is_cached = True
             self._is_cached_valid = current_hash == old_hash
         else:
@@ -133,67 +143,61 @@ class CodePackage:
 
     def copy_code(self, output_dir):
         FILES = {
-            'python': ['*.py', 'requirements.txt*'],
-            'nodejs': ['*.js', 'package.json']
+            "python": ["*.py", "requirements.txt*"],
+            "nodejs": ["*.js", "package.json"],
         }
         path = os.path.join(self.benchmark_path, self._language)
         for file_type in FILES[self._language]:
-            for f in glob.glob( os.path.join(path, file_type) ):
-                shutil.copy2( os.path.join(path, f), output_dir)
+            for f in glob.glob(os.path.join(path, file_type)):
+                shutil.copy2(os.path.join(path, f), output_dir)
 
     def add_benchmark_data(self, output_dir):
-        cmd = '/bin/bash {benchmark_path}/init.sh {output_dir} false'
-        paths = [
-            self.benchmark_path,
-            os.path.join(self.benchmark_path, self._language)
-        ]
+        cmd = "/bin/bash {benchmark_path}/init.sh {output_dir} false"
+        paths = [self.benchmark_path, os.path.join(self.benchmark_path, self._language)]
         for path in paths:
-            if os.path.exists( os.path.join(path, 'init.sh') ):
+            if os.path.exists(os.path.join(path, "init.sh")):
                 out = subprocess.run(
-                        cmd.format(
-                            benchmark_path=path,
-                            output_dir=output_dir
-                        ),
-                        shell=True,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.STDOUT
-                    )
-                logging.debug(out.stdout.decode('utf-8'))
+                    cmd.format(benchmark_path=path, output_dir=output_dir),
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                )
+                logging.debug(out.stdout.decode("utf-8"))
 
     def add_deployment_files(self, output_dir):
-        if 'deployment' in self._system_config:
+        if "deployment" in self._system_config:
             handlers_dir = project_absolute_path(
-                'benchmarks', 'wrappers', self._deployment, self._language
+                "benchmarks", "wrappers", self._deployment, self._language
             )
             handlers = [
                 os.path.join(handlers_dir, file)
-                for file in self._system_config['deployment']['files']
+                for file in self._system_config["deployment"]["files"]
             ]
             for file in handlers:
-                shutil.copy2(file, 'code')
+                shutil.copy2(file, "code")
 
     def add_deployment_package_python(self, output_dir):
         # append to the end of requirements file
-        packages = self._system_config['deployment']['packages']
+        packages = self._system_config["deployment"]["packages"]
         if len(packages):
-            with open(os.path.join(output_dir, 'requirements.txt'), 'a') as out:
+            with open(os.path.join(output_dir, "requirements.txt"), "a") as out:
                 for package in packages:
                     out.write(package)
 
     def add_deployment_package_nodejs(self, output_dir):
         # modify package.json
-        packages = self._system_config['deployment']['packages']
+        packages = self._system_config["deployment"]["packages"]
         if len(packages):
-            package_config = os.path.join(output_dir, 'package.json')
-            package_json = json.load(open(package_config, 'r'))
+            package_config = os.path.join(output_dir, "package.json")
+            package_json = json.load(open(package_config, "r"))
             for key, val in packages.items():
-                package_json['dependencies'][key] = val
-            json.dump(package_json, open(package_config, 'w'), indent=2)
+                package_json["dependencies"][key] = val
+            json.dump(package_json, open(package_config, "w"), indent=2)
 
     def add_deployment_package(self, output_dir):
-        if self._language == 'python':
+        if self._language == "python":
             self.add_deployment_package_python(output_dir)
-        elif self._language == 'nodejs':
+        elif self._language == "nodejs":
             self.add_deployment_package_nodejs(output_dir)
         else:
             raise NotImplementedError
@@ -201,69 +205,71 @@ class CodePackage:
     @staticmethod
     def directory_size(directory: str):
         from pathlib import Path
+
         root = Path(directory)
-        sizes = [f.stat().st_size for f in root.glob('**/*') if f.is_file()]
+        sizes = [f.stat().st_size for f in root.glob("**/*") if f.is_file()]
         return sum(sizes)
 
     def install_dependencies(self, output_dir):
         # do we have docker image for this run and language?
-        if 'build' not in self._system_config['images']:
+        if "build" not in self._system_config["images"]:
             logging.info(
-                ('Docker build image for {deployment} run in {language} '
-                'is not available, skipping').format(
-                    deployment=self._deployment,
-                    language=self._language)
+                (
+                    "Docker build image for {deployment} run in {language} "
+                    "is not available, skipping"
+                ).format(deployment=self._deployment, language=self._language)
             )
         else:
-            container_name = 'sebs.build.{deployment}.{language}.{runtime}'.format(
-                    deployment=self._deployment,
-                    language=self._language,
-                    runtime=self._runtime
+            container_name = "sebs.build.{deployment}.{language}.{runtime}".format(
+                deployment=self._deployment,
+                language=self._language,
+                runtime=self._runtime,
             )
             try:
-                img = self._docker_client.images.get(container_name)
-            except docker.errors.ImageNotFound as err:
-                raise RuntimeError('Docker build image {} not found!'.format(container_name))
+                self._docker_client.images.get(container_name)
+            except docker.errors.ImageNotFound:
+                raise RuntimeError(
+                    "Docker build image {} not found!".format(container_name)
+                )
 
             # does this benchmark has package.sh script?
             volumes = {}
-            package_script = os.path.abspath(os.path.join(
-                        self._benchmark_path, self._language, 'package.sh'
-            ))
+            package_script = os.path.abspath(
+                os.path.join(self._benchmark_path, self._language, "package.sh")
+            )
             if os.path.exists(package_script):
-                volumes[package_script] = {'bind': '/mnt/function/package.sh', 'mode': 'ro'}
+                volumes[package_script] = {
+                    "bind": "/mnt/function/package.sh",
+                    "mode": "ro",
+                }
 
             # run Docker container to install packages
-            PACKAGE_FILES = {
-                'python': 'requirements.txt',
-                'nodejs': 'package.json'
-            }
-            file = os.path.join('code', PACKAGE_FILES[self._language])
+            PACKAGE_FILES = {"python": "requirements.txt", "nodejs": "package.json"}
+            file = os.path.join("code", PACKAGE_FILES[self._language])
             if os.path.exists(file):
                 try:
                     stdout = self._docker_client.containers.run(
                         container_name,
                         volumes={
                             **volumes,
-                            os.path.abspath('code') : {
-                                'bind': '/mnt/function',
-                                'mode': 'rw'
-                            }
+                            os.path.abspath("code"): {
+                                "bind": "/mnt/function",
+                                "mode": "rw",
+                            },
                         },
-                        environment={
-                            'APP': self.benchmark
-                        },
-                        user='1000:1000',
+                        environment={"APP": self.benchmark},
+                        user="1000:1000",
                         remove=True,
-                        stdout=True, stderr=True
+                        stdout=True,
+                        stderr=True,
                     )
                     # Pass to output information on optimizing builds.
                     # Useful for AWS where packages have to obey size limits.
-                    for line in stdout.decode('utf-8').split('\n'):
-                        if 'size' in line:
-                            logging.info('Docker build: {}'.format(line))
+                    for line in stdout.decode("utf-8").split("\n"):
+                        if "size" in line:
+                            logging.info("Docker build: {}".format(line))
                 except docker.errors.ContainerError as e:
-                    logging.error('Package build failed!')
+                    logging.error("Package build failed!")
                     logging.error(e)
                     raise e
 
@@ -273,7 +279,7 @@ class CodePackage:
 
     def build(self, output_dir):
 
-        self._output_dir = os.path.join(output_dir, 'code')
+        self._output_dir = os.path.join(output_dir, "code")
         # create directory to be deployed
         if os.path.exists(self._output_dir):
             shutil.rmtree(self._output_dir)
@@ -286,11 +292,14 @@ class CodePackage:
         self.install_dependencies(self._output_dir)
 
         self._code_size = CodePackage.directory_size(self._output_dir)
-        logging.info('Created code package for run on {deployment} with {language}:{runtime}'.format(
-            deployment=self._deployment,
-            language=self._language,
-            runtime=self._runtime
+        logging.info(
+            (
+                "Created code package for run on {deployment}"
+                + " with {language}:{runtime}"
+            ).format(
+                deployment=self._deployment,
+                language=self._language,
+                runtime=self._runtime,
             )
         )
         return os.path.abspath(self._output_dir)
-
