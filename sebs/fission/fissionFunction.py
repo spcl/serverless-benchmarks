fd24ea31fe4061814c23e7fbc67aa1f71eeff959
Marcin <marcind70@gmail.com>
2020-06-06 22:52:02 +0200

    Add making request

--- sebs/fission/fissionFunction.py
+++ sebs/fission/fissionFunction.py
@@ -2,23 +2,10 @@ from abc import ABC
 from enum import Enum
 from sebs.faas.function import Function
 from typing import List
-
-
-class Trigger(ABC):
-    class TriggerType(Enum):
-        HTTP = 0
-        STORAGE = 1
-
-    def sync_invoke(self):
-        pass
-
-    def async_invoke(self):
-        pass
+import subprocess
 
 
 class FissionFunction(Function):
-    _triggers: List[Trigger]
-
     def __init__(self, name: str):
         self._name = name
 
@@ -27,7 +14,7 @@ class FissionFunction(Function):
         return self._name
 
     def sync_invoke(self, payload: dict):
-        raise Exception("Non-trigger invoke not supported!")
+        subprocess.call(["./run_fission_function.sh", self.name])
 
     def async_invoke(self, payload: dict):
         raise Exception("Non-trigger invoke not supported!")
