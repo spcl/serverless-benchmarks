36330f91c64abb5d517c9451496d8db617a181c7
mcopik <mcopik@gmail.com>
2020-06-11 16:25:51 +0200

    Rename test files to properly return exit code

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import unittest
 import sys
 
 PROJECT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir)
@@ -14,8 +15,10 @@ args = parser.parse_args()
 if not args.deployment:
     args.deployment = []
 
+return_value = True
 
+runner = unittest.TextTestRunner()
 if "aws" in args.deployment:
-    from aws import runner
-
-    runner.run()
+    from aws import suite
+    return_value = return_value and not runner.run(suite.suite()).wasSuccessful()
+sys.exit(return_value)
