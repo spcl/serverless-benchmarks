315e8b01652b0b91e6dd1e07e39a58bdf1382c7d
Marcin Copik <mcopik@gmail.com>
2020-06-18 15:54:19 +0200

    Reimplement installation scripts

---
+++ install.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser(description="Install SeBS and dependencies.")
+parser.add_argument("--with-pypapi", action="store_true")
+args = parser.parse_args()
+
+def execute(cmd):
+    ret = subprocess.run(
+        cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    if ret.returncode:
+        raise RuntimeError(
+            "Running {} failed!\n Output: {}".format(cmd, ret.stdout.decode("utf-8"))
+        )
+    return ret.stdout.decode("utf-8")
+
+env_dir="sebs-virtualenv"
+
+print("Creating Python virtualenv at {}".format(env_dir))
+execute("python3 -mvenv {}".format(env_dir))
+
+print("Install Python dependencies with pip")
+execute("pip3 install -r requirements.txt")
+
+print("Initialize git submodules")
+execute("git submodule update --init --recursive")
+
+if args.with_pypapi:
+    print("Build and install pypapi")
+    cur_dir = os.getcwd()
+    os.chdir(os.path.join("third-party", "pypapi"))
+    execute("git checkout low_api_overflow")
+    execute("pip3 install -r requirements.txt")
+    execute("python3 setup.py build")
+    execute("python3 pypapi/papi_build.py")
+    os.chdir(cur_dir)
