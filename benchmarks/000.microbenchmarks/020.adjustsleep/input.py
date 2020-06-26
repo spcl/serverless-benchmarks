3caaa1246fbd00cc4ecc38df966cd27534114712
marcin <marcind70@gmail.com>
2020-06-26 21:44:52 +0200

    remove unnecessary benchmark

--- benchmarks/000.microbenchmarks/020.adjustsleep/input.py
+++
@@ -1,5 +0,0 @@
-def buckets_count():
-    return (0, 0)
-
-def generate_input(data_dir, size, input_buckets, output_buckets, upload_func):
-    return 0
--- benchmarks/000.microbenchmarks/020.adjustsleep/python/function.py
+++
@@ -1,5 +0,0 @@
-
-def handler():
-    for i in range(10000):
-    	print(i)
-    return "Done"
