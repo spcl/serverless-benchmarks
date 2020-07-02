25de6ea80fc6caf01227c87b2562ae93d58e2b78
marcin <marcind70@gmail.com>
2020-07-03 01:32:34 +0200

    lint wrappers

--- benchmarks/wrappers/fission/python/storage.py
+++ benchmarks/wrappers/fission/python/storage.py
@@ -1,10 +1,10 @@
-import io
 import os
 import uuid
 import json
-import sys
 import minio
 from flask import current_app
+
+
 class storage:
     instance = None
     client = None
@@ -14,21 +14,20 @@ class storage:
         minioConfig = json.load(file)
         try:
             self.client = minio.Minio(
-                    minioConfig["url"],
-                    access_key=minioConfig["access_key"],
-                    secret_key=minioConfig["secret_key"],
-                    secure=False)
+                minioConfig["url"],
+                access_key=minioConfig["access_key"],
+                secret_key=minioConfig["secret_key"],
+                secure=False,
+            )
         except Exception as e:
             current_app.logger.info(e)
-        
+
     @staticmethod
     def unique_name(name):
-        name, extension = name.split('.')
-        return '{name}.{random}.{extension}'.format(
-                    name=name,
-                    extension=extension,
-                    random=str(uuid.uuid4()).split('-')[0]
-                )
+        name, extension = name.split(".")
+        return "{name}.{random}.{extension}".format(
+            name=name, extension=extension, random=str(uuid.uuid4()).split("-")[0]
+        )
 
     def upload(self, bucket, file, filepath):
         key_name = storage.unique_name(file)
@@ -46,7 +45,9 @@ class storage:
 
     def upload_stream(self, bucket, file, bytes_data):
         key_name = storage.unique_name(file)
-        self.client.put_object(bucket, key_name, bytes_data, bytes_data.getbuffer().nbytes)
+        self.client.put_object(
+            bucket, key_name, bytes_data, bytes_data.getbuffer().nbytes
+        )
         return key_name
 
     def download_stream(self, bucket, file):
