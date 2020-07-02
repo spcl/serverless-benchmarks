d402e7a33236109407a9c0c7cab88e279dcf119e
Marcin Copik <mcopik@gmail.com>
2020-07-02 14:46:05 +0200

    Update experiments API

--- sebs/experiments/result.py
+++ sebs/experiments/result.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from typing import Dict, Optional  # noqa
+from typing import Dict, Optional, Tuple  # noqa
 
 from sebs.faas.config import Config as DeploymentConfig
-from sebs.faas.function import ExecutionResult
+from sebs.faas.function import Function, ExecutionResult
 from sebs.experiments.config import Config as ExperimentConfig
 
 
@@ -14,22 +14,28 @@ class Result:
             "experiments": experiment_config,
             "deployment": deployment_config,
         }
-        self.invocations: Dict[str, Dict[str, ExecutionResult]] = {}
+        self._invocations: Dict[str, Dict[str, ExecutionResult]] = {}
         self.result_bucket: Optional[str] = None
 
-    def begin(self):
+    def begin(self) -> float:
         self.begin_time = datetime.now().timestamp()
 
-    def end(self):
+    def end(self) -> float:
         self.end_time = datetime.now().timestamp()
+
+    def times(self) -> Tuple[int, int]:
+        return self.begin_time, self.end_time
 
     def add_result_bucket(self, result_bucket: str):
         self.result_bucket = result_bucket
 
-    def add_invocation(self, func_name: str, invocation: ExecutionResult):
-        if func_name in self.invocations:
-            self.invocations.get(func_name)[  # type: ignore
+    def add_invocation(self, func: Function, invocation: ExecutionResult):
+        if func.name in self._invocations:
+            self._invocations.get(func.name)[  # type: ignore
                 invocation.request_id
             ] = invocation
         else:
-            self.invocations[func_name] = {invocation.request_id: invocation}
+            self._invocations[func.name] = {invocation.request_id: invocation}
+
+    def invocations(self, func: Function) -> Dict[str, ExecutionResult]:
+        return self._invocations[func.name]
