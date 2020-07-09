d9a112d0ea1f32fdb6da89cd2e24e9ebfa3f2336
Marcin Copik <mcopik@gmail.com>
2020-07-09 03:19:52 +0200

    Update tests

--- tests/aws/suite.py
+++ tests/aws/suite.py
@@ -8,11 +8,13 @@ import sebs
 
 from .create_function import AWSCreateFunction
 from .invoke_function_sdk import AWSInvokeFunctionSDK
+from .invoke_function_http import AWSInvokeFunctionHTTP
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(AWSCreateFunction))
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(AWSInvokeFunctionSDK))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(AWSInvokeFunctionHTTP))
     return suite
 
 def run():
