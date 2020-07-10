65625b5bf9bb2041c8c29d416deb0d190052b40b
sborkows <borkowskiszymon28@gmail.com>
2020-07-10 03:17:09 +0200

    get_function in progress

--- sebs/openwhisk/openwhisk.py
+++ sebs/openwhisk/openwhisk.py
@@ -5,12 +5,12 @@ import json
 import os
 from typing import Tuple
 
-import sebs.benchmark
+from sebs import Benchmark
 from sebs.faas import System, PersistentStorage
 from sebs.faas.config import Config
 from sebs.faas.function import Function
+from sebs.openwhisk.openwhiskFunction import OpenwhiskFunction
 from .config import OpenWhiskConfig
-
 
 
 class OpenWhisk(System):
@@ -101,7 +101,7 @@ class OpenWhisk(System):
     def name() -> str:
         return "openwhisk"
 
-    def package_code(self, benchmark: sebs.Benchmark) -> Tuple[str, int]:
+    def package_code(self, benchmark: Benchmark) -> Tuple[str, int]:
 
         benchmark.build()
         node = 'nodejs'
@@ -141,3 +141,95 @@ class OpenWhisk(System):
         logging.info("Created {} archive".format(benchmark_archive))
         bytes_size = os.path.getsize(benchmark_archive)
         return benchmark_archive, bytes_size
+
+    def get_function(self, code_package: Benchmark) -> Function:
+
+        if (
+            code_package.language_version
+            not in self.system_config.supported_language_versions(self.name(), code_package.language_name)
+        ):
+            raise Exception(
+                "Unsupported {language} version {version} in Openwhisk!".format(
+                    language=code_package.language_name,
+                    version=code_package.language_version,
+                )
+            )
+
+        benchmark = code_package.benchmark
+        func_name = code_package.cached_config["name"]
+        code_location = code_package.code_location
+
+        if code_package.is_cached and code_package.is_cached_valid:
+            logging.info(
+                "Using cached function {fname} in {loc}".format(
+                    fname=func_name, loc=code_location
+                )
+            )
+            return OpenwhiskFunction(func_name)
+        elif code_package.is_cached:
+
+            timeout = code_package.benchmark_config.timeout
+            memory = code_package.benchmark_config.memory
+
+            # Run Openwhisk-specific part of building code.
+            package, code_size = self.package_code(code_package)
+
+            self.update_function(
+                benchmark, func_name, package, code_size, timeout, memory
+            )
+
+            cached_cfg = code_package.cached_config
+            cached_cfg["code_size"] = code_size
+            cached_cfg["timeout"] = timeout
+            cached_cfg["memory"] = memory
+            cached_cfg["hash"] = code_package.hash
+            self.cache_client.update_function(
+                self.name(), benchmark, code_package.language_name, package, cached_cfg
+            )
+            # FIXME: fix after dissociating code package and benchmark
+            code_package.query_cache()
+
+            logging.info(
+                "Updating cached function {fname} in {loc}".format(
+                    fname=func_name, loc=code_location
+                )
+            )
+
+            return OpenwhiskFunction(func_name)
+        # no cached instance, create package and upload code
+        else:
+
+            language = code_package.language_name
+            language_runtime = code_package.language_version
+            timeout = code_package.benchmark_config.timeout
+            memory = code_package.benchmark_config.memory
+
+            # Create function name, validation regexp if needed: \A([\w]|[\w][\w@ .-]*[\w@.-]+)\z
+            func_name = "{}-{}-{}".format(benchmark, language, memory)
+
+            package, code_size = self.package_code(code_package)
+            # todo: check if function exists, if so delte otherwise create
+
+            self.cache_client.add_function(
+                deployment=self.name(),
+                benchmark=benchmark,
+                language=language,
+                code_package=package,
+                language_config={
+                    "name": func_name,
+                    "code_size": code_size,
+                    "runtime": language_runtime,
+                    "memory": memory,
+                    "timeout": timeout,
+                    "hash": code_package.hash,
+                },
+                storage_config={
+                    "buckets": {
+                        "input": self.storage.input_buckets,
+                        "output": self.storage.output_buckets,
+                    }
+                },
+            )
+            # FIXME: fix after dissociating code package and benchmark
+            code_package.query_cache()
+            return OpenwhiskFunction(func_name)
