38dbb8f38f5d243aeb520c74db061104391e76b6
Rafal Mucha <mucha.rafal44@gmail.com>
2020-07-11 14:17:42 +0200

    unify config in disc benchmark

--- benchmark_generator/disc/async_nodejs/function.js
+++ benchmark_generator/disc/async_nodejs/function.js
@@ -1,7 +1,7 @@
 //#test
 var result = {};
 var config = {
-    "entries_number": 1000
+    "block_size": 1024*1024*128
 };
 var number = 0;
 //#import
@@ -9,35 +9,30 @@ var fs = require('fs');
 var uuid = require('uuid');
 var uuidv1 = uuid.v1;
 //#function
-function generate_data(entries_number) {
-    var dictToFill = {};
-    for(var i = 0;i < entries_number;i++) {
-        dictToFill[uuidv1()] = uuidv1()
-    }
-    return dictToFill
+function generate_data_disc(block_size) {
+    return Array(block_size + 1).join('x')
 }
-async function testDisc(entries_number) {
+async function testDisc(block_size) {
     try {
-        var data = generate_data(entries_number);
+        var data = generate_data_disc(entries_number);
         var path = "/tmp/serverless-benchmark-test-file.json";
-        var dataAsString = JSON.stringify(data);
         var t0 = new Date();
-        fs.writeFile(path, dataAsString)
+        fs.writeFileSync(path, data);
         var t1 = new Date();
-        fs.readFile(path)
-        var t2 = new Date()
+        await fs.readFile(path);
+        var t2 = new Date();
         return { 
             "write_time" : t1 - t0,
             "read_time": t2 - t1,
-            "bytes": dataAsString.length
+            "bytes": block_size
         }
     } catch (error) {
         return { "error": error.toString() }
     }
 };
 //#run
-var entries_number = config.entries_number;
-await testDisc(entries_number).then(returnJson => {
+var block_size = config.block_size;
+await testDisc(block_size).then(returnJson => {
     result[number] = returnJson;
   }
 );
