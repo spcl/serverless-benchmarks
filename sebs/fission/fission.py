99339758c5cd2cae432507c90fe0fb617398b9b1
Jakub Pajor <jay.pajor@gmail.com>
2020-07-14 22:20:24 +0200

    [feature_fission] fission installation through install.py

--- sebs/fission/fission.py
+++ sebs/fission/fission.py
@@ -15,6 +15,8 @@ from sebs.fission.fissionFunction import FissionFunction
 from sebs.benchmark import Benchmark
 from sebs.fission.config import FissionConfig
 from sebs.fission.minio import Minio
+from tools.fission_preparation import check_if_minikube_installed, run_minikube, check_if_k8s_installed, \
+    check_if_helm_installed, stop_minikube
 
 
 class Fission(System):
@@ -54,146 +56,6 @@ class Fission(System):
         return self._config
 
     @staticmethod
-    def check_if_minikube_installed():
-        try:
-            subprocess.run(
-                "minikube version".split(),
-                check=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-        except subprocess.CalledProcessError:
-            logging.error('ERROR: "minikube" required.')
-
-    @staticmethod
-    def run_minikube(vm_driver="docker"):
-        try:
-            kube_status = subprocess.run(
-                "minikube status".split(), stdout=subprocess.PIPE
-            )
-
-            # if minikube is already running,
-            # error will be raised to prevent to be started minikube again.
-            subprocess.run(
-                "grep Stopped".split(),
-                check=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                input=kube_status.stdout,
-            )
-            try:
-                logging.info("Starting minikube...")
-                subprocess.run(
-                    f"minikube start --vm-driver={vm_driver}".split(), check=True
-                )
-            except subprocess.CalledProcessError:
-                raise ChildProcessError
-
-        except subprocess.CalledProcessError:
-            try:
-                subprocess.run(
-                    "grep unusually".split(),
-                    check=True,
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL,
-                    input=kube_status.stdout,
-                )
-                logging.info("Starting minikube...")
-                subprocess.run(
-                    f"minikube start --vm-driver={vm_driver}".split(), check=True
-                )
-            except subprocess.CalledProcessError:
-                logging.info("Minikube already working")
-            pass
-        except ChildProcessError:
-            logging.error("ERROR: COULDN'T START MINIKUBE")
-            exit(1)
-
-    @staticmethod
-    def check_if_k8s_installed():
-        try:
-            subprocess.run(
-                "kubectl version".split(),
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                check=True,
-            )
-        except subprocess.CalledProcessError:
-            logging.error('ERROR: "kubectl" required.')
-
-    @staticmethod
-    def check_if_helm_installed():
-        try:
-            subprocess.run(
-                "helm version".split(),
-                check=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-        except subprocess.CalledProcessError:
-            logging.error('ERROR: "helm" required.')
-
-    @staticmethod
-    def install_fission_using_helm(k8s_namespace="fission"):
-        fission_url = "https://github.com/fission/fission/releases/download/1.9.0/fission-all-1.9.0.tgz"  # noqa: E501
-
-        try:
-            k8s_namespaces = subprocess.run(
-                "kubectl get namespace".split(),
-                stdout=subprocess.PIPE,
-                stderr=subprocess.DEVNULL,
-            )
-            subprocess.run(
-                f"grep {k8s_namespace}".split(),
-                check=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                input=k8s_namespaces.stdout,
-            )
-            logging.info("fission namespace already exist")
-        except (subprocess.CalledProcessError):
-            logging.info(
-                f'No proper fission namespace.Installing Fission as "{k8s_namespace}.."'
-            )
-            subprocess.run(
-                f"kubectl create namespace {k8s_namespace}".split(), check=True
-            )
-            subprocess.run(
-                f"helm install --namespace {k8s_namespace} \
-                    --name-template fission {fission_url}".split(),
-                check=True,
-            )
-
-    @staticmethod
-    def install_fission_cli_if_needed():
-        try:
-            subprocess.run(
-                ["fission"],
-                check=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                shell=True,
-            )
-        except subprocess.CalledProcessError:  # fission cli is not installed
-            logging.info("No fission CLI - installing...")
-            available_os = {"darwin": "osx", "linux": "linux"}
-            import platform
-
-            fission_cli_url = (
-                f"https://github.com/fission/fission/releases/download/1.9.0/fission-cli-"  # noqa: E501
-                f"{available_os[platform.system().lower()]}"
-            )
-
-            subprocess.run(
-                f"curl -Lo fission {fission_cli_url} \
-                && chmod +x fission \
-                && sudo mv fission /usr/local/bin/",
-                stdout=subprocess.DEVNULL,
-                check=True,
-                shell=True,
-            )
-
-    @staticmethod
     def add_port_forwarding(port=5051):
         podName = (
             subprocess.run(
@@ -221,7 +83,7 @@ class Fission(System):
             subprocess.run(f"fission env delete --name {self.envName}".split())
         self.storage.storage_container.kill()
         logging.info("Minio stopped")
-        subprocess.run(f"minikube stop".split())
+        stop_minikube()
 
     def get_storage(self, replace_existing: bool = False) -> PersistentStorage:
         self.storage = Minio(self.docker_client)
@@ -231,12 +93,10 @@ class Fission(System):
         if config is None:
             config = {}
 
-        Fission.check_if_minikube_installed()
-        Fission.run_minikube()
-        Fission.check_if_k8s_installed()
-        Fission.check_if_helm_installed()
-        Fission.install_fission_using_helm()
-        Fission.install_fission_cli_if_needed()
+        check_if_minikube_installed()
+        check_if_k8s_installed()
+        check_if_helm_installed()
+        run_minikube()
         Fission.add_port_forwarding()
         sleep(5)
 
