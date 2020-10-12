5284cf289c3c1566f8ce68fd3a3ddd845bd5c4d5
Marcin Copik <mcopik@gmail.com>
2020-10-13 00:18:09 +0200

    simplify CLI

--- sebs.py
+++ sebs.py
@@ -9,6 +9,7 @@ import traceback
 from typing import Optional
 
 import sebs
+from sebs.faas.function import Trigger
 
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -16,76 +17,37 @@ parser = argparse.ArgumentParser(description="Run cloud experiments.")
 parser.add_argument(
     "action",
     choices=[
-        "publish",
         "invoke",
         "download_metrics",
         "experiment",
         "process_experiment",
-        "create",
-        "results",
-        "logs",
-        "burst_invoke",
     ],
     help="Benchmark name",
 )
-parser.add_argument("benchmark", type=str, help="Benchmark name")
+#FIXME: add invoke of function with repetitions, benchmark size
+#parser.add_argument("benchmark", type=str, help="Benchmark name")
+#parser.add_argument(
+#    "size", choices=["test", "small", "large"], help="Benchmark input test size"
+#)
 parser.add_argument("output_dir", type=str, help="Output dir")
-parser.add_argument(
-    "size", choices=["test", "small", "large"], help="Benchmark input test size"
-)
 parser.add_argument("config", type=str, help="Config JSON for experiments")
 parser.add_argument(
     "--deployment", choices=["azure", "aws", "local"], help="Cloud to use"
 )
-parser.add_argument("--experiment", choices=["time_warm"], help="Experiment to run")
-parser.add_argument(
-    "--language", choices=["python", "nodejs"], default=None, help="Benchmark language"
-)
-parser.add_argument(
-    "--language-version", type=str, default=None, help="Benchmark language version"
-)
-parser.add_argument(
-    "--repetitions",
-    action="store",
-    default=5,
-    type=int,
-    help="Number of experimental repetitions",
-)
-# TODO: make JSON config
-parser.add_argument(
-    "--invocations", action="store", type=int, help="Number of experimental repetitions"
-)
-parser.add_argument(
-    "--times-begin-idx",
-    action="store",
-    type=int,
-    help="Number of experimental repetitions",
-)
-parser.add_argument(
-    "--times-end-idx",
-    action="store",
-    type=int,
-    help="Number of experimental repetitions",
-)
-parser.add_argument(
-    "--config-experiment-runner",
-    action="store",
-    type=str,
-    help="Number of experimental repetitions",
-)
-parser.add_argument(
-    "--sleep-time", action="store", type=int, help="Number of experimental repetitions"
-)
-parser.add_argument(
-    "--memory", action="store", type=int, help="Number of experimental repetitions"
-)
-parser.add_argument(
-    "--extend",
-    action="store",
-    type=str,
-    default=None,
-    help="Number of experimental repetitions",
-)
+#parser.add_argument("--experiment", choices=["time_warm"], help="Experiment to run")
+#parser.add_argument(
+#    "--language", choices=["python", "nodejs"], default=None, help="Benchmark language"
+#)
+#parser.add_argument(
+#    "--language-version", type=str, default=None, help="Benchmark language version"
+#)
+#parser.add_argument(
+#    "--repetitions",
+#    action="store",
+#    default=5,
+#    type=int,
+#    help="Number of experimental repetitions",
+#)
 parser.add_argument(
     "--cache", action="store", default="cache", type=str, help="Cache directory"
 )
@@ -122,9 +84,6 @@ parser.add_argument(
 )
 parser.add_argument(
     "--verbose", action="store", default=False, type=bool, help="Verbose output"
-)
-parser.add_argument(
-    "--experiment-input", action="store", type=str, help="Verbose output"
 )
 args = parser.parse_args()
 
@@ -175,7 +134,7 @@ try:
             logging_filename=logging_filename
         )
 
-        if args.action == "test_invoke":
+        if args.action == "invoke":
             func = deployment_client.get_function(
                 benchmark, deployment_client.default_function_name(benchmark)
             )
@@ -192,7 +151,7 @@ try:
                 experiment_config, deployment_client.config
             )
             result.begin()
-            ret = func.triggers[0].sync_invoke(input_config)
+            ret = func.triggers.get(Trigger.TriggerType.HTTP).sync_invoke(input_config)
             result.end()
             result.add_invocation(func, ret)
             with open("experiments.json", "w") as out_f:
@@ -227,102 +186,6 @@ try:
         experiment = sebs_client.get_experiment(config["experiments"])
         # TODO: additional params
         experiment.process(output_dir)
-    #        # Prepare benchmark input
-    #        input_config = prepare_input(
-    #            client=deployment_client,
-    #            benchmark=args.benchmark,
-    #            size=args.size,
-    #            update_storage=experiment_config["experiments"]["update_storage"],
-    #        )
-    #        package = CodePackage(
-    #            args.benchmark,
-    #            experiment_config,
-    #            output_dir,
-    #            systems_config[deployment],
-    #            cache_client,
-    #            docker_client,
-    #            args.update,
-    #        )
-    #        assert args.invocations is not None
-    #        assert args.sleep_time
-    #        # TODO: experiment JSON config
-    #        runner = ExperimentRunner(
-    #            not args.no_update_function,
-    #            config_file=args.config_experiment_runner,
-    #            invocations=args.invocations,
-    #            repetitions=args.repetitions,
-    #            sleep_time=args.sleep_time,
-    #            memory=args.memory,
-    #            times_begin_idx=args.times_begin_idx,
-    #            times_end_idx=args.times_end_idx,
-    #            benchmark=args.benchmark,
-    #            output_dir=output_dir,
-    #            language=language,
-    #            input_config=input_config,
-    #            experiment_config=experiment_config,
-    #            code_package=package,
-    #            deployment_client=deployment_client,
-    #            cache_client=cache_client,
-    #        )
-    #    elif args.action == "create":
-    #        # Prepare benchmark input
-    #        input_config = prepare_input(
-    #            client=deployment_client,
-    #            benchmark=args.benchmark,
-    #            size=args.size,
-    #            update_storage=experiment_config["experiments"]["update_storage"],
-    #        )
-    #        package = CodePackage(
-    #            args.benchmark,
-    #            experiment_config,
-    #            output_dir,
-    #            systems_config[deployment],
-    #            cache_client,
-    #            docker_client,
-    #            args.update,
-    #        )
-    #        create_functions(
-    #            deployment_client,
-    #            cache_client,
-    #            package,
-    #            experiment_config,
-    #            args.benchmark,
-    #            language,
-    #            args.memory,
-    #            args.times_begin_idx,
-    #            args.times_end_idx,
-    #            args.sleep_time,
-    #            args.extend,
-    #        )
-    #    elif args.action == "results":
-    #        assert args.experiment_input is not None
-    #        experiment = json.load(open(args.experiment_input, "r"))
-    #        result = get_results(deployment_client, experiment, args.output_dir)
-    #        with open(os.path.join(args.output_dir, "results.json"), "w") as out_f:
-    #            json.dump(result, out_f, indent=2)
-    #    elif args.action == "burst_invoke":
-    #        input_config = prepare_input(
-    #            client=deployment_client,
-    #            benchmark=args.benchmark,
-    #            size=args.size,
-    #            update_storage=experiment_config["experiments"]["update_storage"],
-    #        )
-    #        begin = datetime.datetime.now()
-    #        result = run_burst_experiment(
-    #            config_file=args.config_experiment_runner,
-    #            invocations=args.invocations,
-    #            memories=[128, 256, 512, 1024],  # , 1536, 1792, 2048, 3092],
-    #            repetitions=args.repetitions,
-    #            benchmark=args.benchmark,
-    #            output_dir=output_dir,
-    #            language=language,
-    #            input_config=input_config,
-    #            experiment_config=experiment_config,
-    #            deployment_client=deployment_client,
-    #            cache_client=cache_client,
-    #            additional_cfg=experiment_config,
-    #        )
-    #        end = datetime.datetime.now()
     else:
         raise RuntimeError("Unknown option")
 
