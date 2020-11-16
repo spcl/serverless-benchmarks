a9fb2a3ce1c898e4ab5ac9c9d6dcbe7fc85234aa
Marcin Copik <mcopik@gmail.com>
2020-11-16 15:59:39 +0100

    Fix bug in 110.dynamic-html

--- benchmarks/100.webapps/110.dynamic-html/python/function.py
+++ benchmarks/100.webapps/110.dynamic-html/python/function.py
@@ -14,7 +14,7 @@ def handler(event):
     name = event.get('username')
     size = event.get('random_len')
     cur_time = datetime.now()
-    random_numbers = sample(range(0, 100), size)
+    random_numbers = sample(range(0, 1000000), size)
     template = Template( open(path.join(SCRIPT_DIR, 'templates', 'template.html'), 'r').read())
     html = template.render(username = name, cur_time = cur_time, random_numbers = random_numbers)
     # end timing
