612307522e9815e5b095c19d91a84f7916528cfb
Marcin Copik <mcopik@gmail.com>
2022-05-04 01:59:49 +0200

    [system] Avoid failures on Docker images with existing group

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ GROUP_ID=${CONTAINER_GID}
 USER=${CONTAINER_USER}
 
 useradd --non-unique -m -u ${USER_ID} ${USER}
-groupmod -g ${GROUP_ID} ${USER}
+groupmod --non-unique -g ${GROUP_ID} ${USER}
 mkdir -p /mnt/function && chown -R ${USER}:${USER} /mnt/function
 export HOME=/home/${USER}
 echo "Running as ${USER}, with ${USER_ID} and ${GROUP_ID}"
