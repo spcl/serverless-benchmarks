7b38dd03818ed696d57c5a7211f7f5815180049f
Marcin Copik <mcopik@gmail.com>
2022-05-14 22:49:34 +0200

    [system] Add first, incomplete version of P2P experiment

---
+++ sebs/experiments/communication_p2p.py
@@ -0,0 +1,117 @@
+import concurrent
+import os
+import uuid
+from typing import TYPE_CHECKING
+
+from sebs.faas.system import System as FaaSSystem
+from sebs.experiments.experiment import Experiment
+from sebs.experiments.config import Config as ExperimentConfig
+from sebs.experiments.result import Result as ExperimentResult
+from sebs.utils import serialize
+
+if TYPE_CHECKING:
+    from sebs import SeBS
+
+
+class CommunicationP2P(Experiment):
+    def __init__(self, config: ExperimentConfig):
+        super().__init__(config)
+        self.settings = self.config.experiment_settings(self.name())
+
+    def prepare(self, sebs_client: "SeBS", deployment_client: FaaSSystem):
+
+        # deploy network test function
+        from sebs import SeBS  # noqa
+        from sebs.faas.function import Trigger
+
+        experiment_type = self.settings["type"]
+        self._benchmark = sebs_client.get_benchmark(
+            f"051.communication.{experiment_type}", deployment_client, self.config
+        )
+        self._function = deployment_client.get_function(self._benchmark)
+
+        triggers = self._function.triggers(Trigger.TriggerType.LIBRARY)
+        self._trigger = triggers[0]
+
+        self._storage = deployment_client.get_storage(replace_existing=True)
+        self._out_dir = os.path.join(sebs_client.output_dir, "communication-p2p", experiment_type)
+        if not os.path.exists(self._out_dir):
+            os.makedirs(self._out_dir)
+
+        self._experiment_bucket = self._storage.experiments_bucket()
+        self._experiment_id = f"run-{str(uuid.uuid4())[0:8]}"
+
+        self._deployment_client = deployment_client
+
+    def run(self):
+
+        bucket_key = os.path.join(self.name(), self._experiment_id)
+
+        for invocations in self.settings["invocations"]["invocations_per_round"]:
+
+            result = ExperimentResult(self.config, self._deployment_client.config)
+            result.begin()
+
+            self.logging.info(f"Begin experiment with {invocations} invocations per function call")
+
+            input_config = {
+                "bucket": self._experiment_bucket,
+                "key": bucket_key,
+                "size": self.settings["size"],
+                "invocations": {
+                    "warmup": self.settings["invocations"]["warmup"],
+                    "invocations": invocations,
+                    "with_backoff": False,
+                },
+            }
+            total_iters = self.settings["invocations"]["total"]
+            invocations_processed = 0
+
+            pool = concurrent.futures.ThreadPoolExecutor()
+
+            while invocations_processed < total_iters:
+
+                self.logging.info(f"Invoking {invocations} repetitions")
+
+                current_input = input_config
+                current_input["invocations"]["offset"] = invocations_processed
+
+                # FIXME: propert implementation in language triggers
+                fut = pool.submit(self._trigger.sync_invoke, {**current_input, "role": "producer"})
+                consumer = self._trigger.sync_invoke({**current_input, "role": "consumer"})
+                result.add_invocation(self._function, consumer)
+                result.add_invocation(self._function, fut.result())
+
+                invocations_processed += invocations
+
+                self.logging.info(f"Finished {invocations_processed}/{total_iters}")
+
+            result.end()
+
+            results_config = {
+                "type": "storage",
+                "benchmark_input": input_config,
+                "bucket": self._experiment_bucket,
+                "experiment_id": self._experiment_id,
+            }
+
+            file_name = f"invocations_{invocations}_results.json"
+            with open(os.path.join(self._out_dir, file_name), "w") as out_f:
+                out_f.write(serialize({"experiment": result, "results": results_config}))
+
+    def process(
+        self,
+        sebs_client: "SeBS",
+        deployment_client,
+        directory: str,
+        logging_filename: str,
+    ):
+        pass
+
+    @staticmethod
+    def name() -> str:
+        return "communication-p2p"
+
+    @staticmethod
+    def typename() -> str:
+        return "Experiment.CommunicationP2P"
