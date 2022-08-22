6ab6c74e3d5bfb2817c5483bc038d814968a05b0
Marcin Copik <mcopik@gmail.com>
2022-08-22 16:41:39 +0200

    [sebs] Clear incorrect linting

--- sebs/types.py
+++ sebs/types.py
@@ -2,15 +2,15 @@ from enum import Enum
 
 
 class Platforms(str, Enum):
-    AWS = ("aws",)
-    AZURE = ("azure",)
-    GCP = ("gcp",)
-    LOCAL = ("local",)
+    AWS = "aws"
+    AZURE = "azure"
+    GCP = "gcp"
+    LOCAL = "local"
     OPENWHISK = "openwhisk"
 
 
 class Storage(str, Enum):
-    AWS_S3 = ("aws-s3",)
-    AZURE_BLOB_STORAGE = ("azure-blob-storage",)
-    GCP_STORAGE = ("google-cloud-storage",)
+    AWS_S3 = "aws-s3"
+    AZURE_BLOB_STORAGE = "azure-blob-storage"
+    GCP_STORAGE = "google-cloud-storage"
     MINIO = "minio"
