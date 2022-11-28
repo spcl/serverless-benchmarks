3a0fc9b584eeb1a9c7cd8e8d3f95146922a4a2f5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 18:01:03 +0000

    Adding tarfile member sanitization to extractall()

--- sebs/benchmark.py
+++ sebs/benchmark.py
@@ -438,7 +438,26 @@ class Benchmark(LoggingBase):
                             for chunk in data:
                                 f.write(chunk)
                         with tarfile.open(tar_archive, "r") as tar:
-                            tar.extractall(output_dir)
+                            def is_within_directory(directory, target):
+                                
+                                abs_directory = os.path.abspath(directory)
+                                abs_target = os.path.abspath(target)
+                            
+                                prefix = os.path.commonprefix([abs_directory, abs_target])
+                                
+                                return prefix == abs_directory
+                            
+                            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                            
+                                for member in tar.getmembers():
+                                    member_path = os.path.join(path, member.name)
+                                    if not is_within_directory(path, member_path):
+                                        raise Exception("Attempted Path Traversal in Tar File")
+                            
+                                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                
+                            
+                            safe_extract(tar, output_dir)
                             # docker packs the entire directory with basename function
                             for f in os.listdir(os.path.join(output_dir, "function")):
                                 shutil.move(
