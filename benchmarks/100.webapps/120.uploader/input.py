84fc41b8b48a65e9439af6eaf637f8e2a8ff43f0
Marcin Copik <mcopik@gmail.com>
2023-03-06 14:33:37 +0100

    [benchmarks] Update the URL for HPX 1.4.0 archive used as small input for benchmark 120.uploader

--- benchmarks/100.webapps/120.uploader/input.py
+++ benchmarks/100.webapps/120.uploader/input.py
@@ -3,7 +3,7 @@ url_generators = {
     # source: mlperf fake_imagenet.sh. 230 kB
     'test' : 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Jammlich_crop.jpg/800px-Jammlich_crop.jpg',
     # video: HPX source code, 6.7 MB
-    'small': 'https://github.com/STEllAR-GROUP/hpx/archive/1.4.0.zip',
+    'small': 'https://github.com/STEllAR-GROUP/hpx/archive/refs/tags/1.4.0.zip',
     # resnet model from pytorch. 98M
     'large':  'https://download.pytorch.org/models/resnet50-19c8e357.pth'
 }
