6a233ff720d8bca912b6548f364a1b04fdb55488
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-04-09 11:39:25 +0530

    Update cpp_benchmarks.cpp

--- benchmarks/cpp_benchmarks.cpp
+++ benchmarks/cpp_benchmarks.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -12,9 +11,11 @@
 #include <benchmark/benchmark.h>
 
 void setup(benchmark::State& state) {
+  // Set up compile and run commands for the C++ benchmark
   std::vector<std::string> compile_cmd = {"g++", "-std=c++11", "-O3", "-DNDEBUG", "-I./benchmark/include", "cpp_benchmark.cpp", "-o", "cpp_benchmark"};
   std::vector<std::string> run_cmd = {"./cpp_benchmark", "--benchmark_format=json"};
 
+  // Compile the C++ benchmark
   std::ostringstream compile_out;
   int compile_ret = benchmark::RunSpecifiedCommand(compile_cmd, &compile_out);
   if (compile_ret != 0) {
@@ -22,6 +23,7 @@ void setup(benchmark::State& state) {
     return;
   }
 
+  // Run the C++ benchmark
   std::ostringstream run_out;
   int run_ret = benchmark::RunSpecifiedCommand(run_cmd, &run_out);
   if (run_ret != 0) {
@@ -29,10 +31,10 @@ void setup(benchmark::State& state) {
     return;
   }
 
+  // Parse the output of the C++ benchmark and store the results
   std::string output = run_out.str();
   std::istringstream ss(output);
   std::string line;
-
   while (std::getline(ss, line)) {
     std::istringstream ls(line);
     std::string field, value;
@@ -40,9 +42,11 @@ void setup(benchmark::State& state) {
     std::getline(ls, value, ',');
 
     if (field == "\"name\"") {
+      // Extract the name of the benchmark
       std::string benchmark_name = value.substr(1, value.size() - 2);
       double benchmark_time = 0.0;
 
+      // Extract the real time taken by the benchmark
       while (std::getline(ss, line) && line != "},") {
         std::istringstream ls(line);
         std::string field, value;
@@ -54,10 +58,12 @@ void setup(benchmark::State& state) {
         }
       }
 
+      // Store the benchmark results
       overall_results[benchmark_name]["cpp"] = benchmark_time;
     }
   }
 
+  // Run the benchmarking loop (this loop is empty)
   for (auto _ : state) {}
 }
 
@@ -66,9 +72,10 @@ void setup(benchmark::State& state) {
 int main(int argc, char** argv) {
   // ...
 
-  // Run the C++ benchmark
+  // Register the C++ benchmark and set its unit of measurement
   benchmark::RegisterBenchmark("cpp", setup)->Unit(benchmark::kMillisecond);
 
+  // Initialize the benchmark framework and run the benchmarks
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
 
