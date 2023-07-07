dc12483ec504a81382339a291184188e3b7729a0
Marcin Copik <mcopik@gmail.com>
2023-07-07 12:11:29 -0400

    Support PyTorch benchmark for Python 3.8 and 3.9 on Lambda (#165)
    * [benchmarks] Add additional zipping step for torch versions that are too large for Lambda
    
    * [docker] Add zip to the AWS build image
    
    * [benchmarks] Add unzipping code for torch in benchmark 411.image-recognition
    
    * [benchmarks] Add documentation for double-zipping with PyTorch

--- benchmarks/400.inference/411.image-recognition/python/package.sh
+++ benchmarks/400.inference/411.image-recognition/python/package.sh
@@ -15,11 +15,17 @@ find . -type d -name "bin" -not -path "*/torch/*" -exec rm -rf {} +
 find -name "*.so" -not -path "*/PIL/*" -not -path "*/Pillow.libs/*" | xargs strip
 find -name "*.so.*" -not -path "*/PIL/*" -not -path "*/Pillow.libs/*" | xargs strip
 
-rm -r pip > /dev/null
-rm -r pip-* > /dev/null
-rm -r wheel > /dev/null
-rm -r wheel-* > /dev/null
-rm easy_install.py > /dev/null
+rm -r pip >/dev/null
+rm -r pip-* >/dev/null
+rm -r wheel >/dev/null
+rm -r wheel-* >/dev/null
+rm easy_install.py >/dev/null
 find . -name \*.pyc -delete
 cd ${CUR_DIR}
 echo "Stripped size $(du -sh $1 | cut -f1)"
+
+if [[ "${PYTHON_VERSION}" == "3.8" ]] || [[ "${PYTHON_VERSION}" == "3.9" ]]; then
+	zip -qr torch.zip $1/torch
+	rm -rf $1/torch
+	echo "Torch-zipped size $(du -sh ${CUR_DIR} | cut -f1)"
+fi
