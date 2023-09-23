7bdc95c6bc73ac4543d36b53df6a7fe6124767d7
Marcin Copik <mcopik@gmail.com>
2023-09-24 01:35:41 +0200

    [benchmarks] Adapt the rest of benchmarks to a new design

--- benchmarks/500.scientific/504.dna-visualisation/python/function.py
+++ benchmarks/500.scientific/504.dna-visualisation/python/function.py
@@ -1,4 +1,4 @@
-import datetime, io, json
+import datetime, io, json, os
 # using https://squiggle.readthedocs.io/en/latest/
 from squiggle import transform
 
@@ -7,13 +7,14 @@ client = storage.storage.get_instance()
 
 def handler(event):
 
-    input_bucket = event.get('bucket').get('input')
-    output_bucket = event.get('bucket').get('output')
+    bucket = event.get('bucket').get('bucket')
+    input_prefix = event.get('bucket').get('input')
+    output_prefix = event.get('bucket').get('output')
     key = event.get('object').get('key')
     download_path = '/tmp/{}'.format(key)
 
     download_begin = datetime.datetime.now()
-    client.download(input_bucket, key, download_path)
+    client.download(bucket, os.path.join(input_prefix, key), download_path)
     download_stop = datetime.datetime.now()
     data = open(download_path, "r").read()
 
@@ -24,20 +25,22 @@ def handler(event):
     upload_begin = datetime.datetime.now()
     buf = io.BytesIO(json.dumps(result).encode())
     buf.seek(0)
-    key_name = client.upload_stream(output_bucket, key, buf)
+    key_name = client.upload_stream(bucket, os.path.join(output_prefix, key), buf)
     upload_stop = datetime.datetime.now()
     buf.close()
 
     download_time = (download_stop - download_begin) / datetime.timedelta(microseconds=1)
+    upload_time = (upload_stop - upload_begin) / datetime.timedelta(microseconds=1)
     process_time = (process_end - process_begin) / datetime.timedelta(microseconds=1)
 
     return {
             'result': {
-                'bucket': output_bucket,
+                'bucket': bucket,
                 'key': key_name
             },
             'measurement': {
                 'download_time': download_time,
-                'compute_time': process_time
+                'compute_time': process_time,
+                'upload_time': process_time
             }
     }
