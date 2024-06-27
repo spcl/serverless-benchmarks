defa0421109330054489de97411f30891b5c357e
prajinkhadka <prawjeenkhadka@gmail.com>
2024-06-26 20:07:31 -0500

    Add Architecture paramter when installing npm packages

--- dockerfiles/nodejs_installer.sh
+++ dockerfiles/nodejs_installer.sh
@@ -3,4 +3,5 @@
 if [ -f $FILE ]; then
   . /nvm/nvm.sh
 fi
-cd /mnt/function && npm install && rm -rf package-lock.json
+cd /mnt/function && npm install --target_arch=${TARGET_ARCHITECTURE} && rm -rf package-lock.json
+
