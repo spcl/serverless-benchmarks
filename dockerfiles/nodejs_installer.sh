1d00d99c0d77e2d33b9785a2978999cb6b8539ac
prajinkhadka <prawjeenkhadka@gmail.com>
2024-06-29 23:58:26 -0500

    Add approprivate version for python3.9 arm

--- dockerfiles/nodejs_installer.sh
+++ dockerfiles/nodejs_installer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
+#
 cd /mnt/function
 
-if [ "${TARGET_ARCHITECTURE}" = "arm64" ]; then
-    npm install --arch=arm64
+if [ "${TARGET_ARCHITECTURE}" == "arm64" ]; then
+        npm install --arch=arm64
 elif [ "${TARGET_ARCHITECTURE}" = "x86_64" ]; then
-    npm install --arch=x64
+        npm install --arch=x64
 else
-    echo "Unsupported architecture: $TARGET_ARCHITECTURE"
-    exit 1
+        echo "Unsupported architecture: $TARGET_ARCHITECTURE"
+        exit 1
 fi
 
 rm -rf package-lock.json
-
