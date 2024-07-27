b21cd34d753cd9e988f67e0f0062a569f5c1cf54
Marcin Copik <mcopik@gmail.com>
2024-07-27 21:48:39 +0200

    [aws] Update AWS wrapper with query interface

--- benchmarks/wrappers/aws/python/nosql.py
+++ benchmarks/wrappers/aws/python/nosql.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 
 import boto3
 
@@ -16,27 +17,60 @@ class nosql:
 
         return self._tables[table_name]
 
-    def insert(self, table_name: str, key: str, data: dict):
+    def insert(
+        self,
+        table_name: str,
+        primary_key: Tuple[str, str],
+        secondary_key: Tuple[str, str],
+        data: dict
+    ):
+        for key in (primary_key, secondary_key):
+            data[key[0]] = key[1]
+
         self.get_table(table_name).put_item(
             TableName=table_name,
             Item=data
         )
 
-    def get(self, table_name: str, key_name: str, key: str) -> dict:
+    def get(
+        self,
+        table_name: str,
+        primary_key: Tuple[str, str],
+        secondary_key: Tuple[str, str]
+    ) -> dict:
+
+        data = {}
+        for key in (primary_key, secondary_key):
+            data[key[0]] = key[1]
+
         res = self.get_table(table_name).get_item(
             TableName=table_name,
-            Key={
-                key_name: key
-            }
+            Key=data
         )
         return res['Item']
 
-    def delete(self, table_name: str, key_name: str, key: str):
+    def query(self, table_name: str, primary_key: Tuple[str, str]) -> List[dict]:
+
+        return self.get_table(table_name).query(
+            KeyConditionExpression=f"{primary_key[0]} = :keyvalue",
+            ExpressionAttributeValues={
+                ':keyvalue': primary_key[1]
+            }
+        )['Items']
+
+    def delete(
+        self,
+        table_name: str,
+        primary_key: Tuple[str, str],
+        secondary_key: Tuple[str, str]
+    ):
+        data = {}
+        for key in (primary_key, secondary_key):
+            data[key[0]] = key[1]
+
         self.get_table(table_name).delete_item(
             TableName=table_name,
-            Key={
-                key_name: key
-            }
+            Key=data
         )
 
     @staticmethod
