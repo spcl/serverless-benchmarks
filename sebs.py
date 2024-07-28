8314f410ce121e8fa61d25063d6e86e94a475c12
Marcin Copik <mcopik@gmail.com>
2024-07-29 00:44:19 +0200

    [system] Initialize storage before deploying function

--- sebs.py
+++ sebs.py
@@ -242,12 +242,13 @@ def invoke(
     if timeout is not None:
         benchmark_obj.benchmark_config.timeout = timeout
 
+    storage = deployment_client.get_storage(replace_existing=experiment_config.update_storage)
+    input_config = benchmark_obj.prepare_input(storage=storage, nosql_storage=deployment_client.get_nosql_storage(), size=benchmark_input_size)
+
     func = deployment_client.get_function(
         benchmark_obj,
         function_name if function_name else deployment_client.default_function_name(benchmark_obj),
     )
-    storage = deployment_client.get_storage(replace_existing=experiment_config.update_storage)
-    input_config = benchmark_obj.prepare_input(storage=storage, nosql_storage=deployment_client.get_nosql_storage(), size=benchmark_input_size)
 
     result = sebs.experiments.ExperimentResult(experiment_config, deployment_client.config)
     result.begin()
