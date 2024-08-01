91957e665136db991a2e17d49d6e2ddc7f3617f2
Marcin Copik <mcopik@gmail.com>
2024-08-02 01:37:48 +0200

    [benchmarks] Update to the newest API

--- benchmarks/600.workflows/620.func-invo/input.py
+++ benchmarks/600.workflows/620.func-invo/input.py
@@ -12,8 +12,5 @@ size_generators = {
     '2e18-1000': (2**18)-1000
 }
 
-def buckets_count():
-    return (0, 0)
-
-def generate_input(data_dir, size, input_buckets, output_buckets, upload_func):
+def generate_input(data_dir, size, benchmarks_bucket, input_paths, output_paths, upload_func, nosql_func):
     return { 'size': size_generators[size] }
