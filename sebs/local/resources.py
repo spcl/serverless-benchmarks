0a678b5635914cef4796fff79ede67a5b8f10081
Marcin Copik <mcopik@gmail.com>
2024-08-07 19:44:04 +0200

    [local] Support ScyllaDB serialization

--- sebs/local/resources.py
+++ sebs/local/resources.py
@@ -6,6 +6,8 @@ from sebs.faas.resources import SystemResources
 from sebs.faas.storage import PersistentStorage
 from sebs.faas.nosql import NoSQLStorage
 from sebs.local.config import LocalConfig, LocalResources
+from sebs.storage.config import MinioConfig, ScyllaDBConfig
+from sebs.storage.scylladb import ScyllaDB
 from sebs.utils import LoggingHandlers
 
 import docker
@@ -23,6 +25,7 @@ class LocalSystemResources(SystemResources):
 
         self._logging_handlers = logger_handlers
         self._storage: Optional[PersistentStorage] = None
+        self._nosql_storage: Optional[NoSQLStorage] = None
 
     """
         Create wrapper object for minio storage and fill buckets.
@@ -44,15 +47,45 @@ class LocalSystemResources(SystemResources):
                 )
                 raise RuntimeError("Cannot run local deployment without any object storage")
 
-            self._storage = Minio.deserialize(
-                storage_config,
-                self._cache_client,
-                self._config.resources,
-            )
-            self._storage.logging_handlers = self._logging_handlers
+            if isinstance(storage_config, MinioConfig):
+                self._storage = Minio.deserialize(
+                    storage_config,
+                    self._cache_client,
+                    self._config.resources,
+                )
+                self._storage.logging_handlers = self._logging_handlers
+            else:
+                self.logging.error(
+                    "The local deployment does not support "
+                    f"the object storage config type: {type(storage_config)}!"
+                )
+                raise RuntimeError("Cannot work with the provided object storage!")
+
         elif replace_existing is not None:
             self._storage.replace_existing = replace_existing
         return self._storage
 
     def get_nosql_storage(self) -> NoSQLStorage:
-        raise NotImplementedError()
+
+        if self._nosql_storage is None:
+
+            storage_config = cast(LocalResources, self._config.resources).nosql_storage_config
+            if storage_config is None:
+                self.logging.error(
+                    "The local deployment is missing the configuration of pre-allocated NoSQL storage!"
+                )
+                raise RuntimeError("Cannot allocate NoSQL storage!")
+
+            if isinstance(storage_config, ScyllaDBConfig):
+                self._nosql_storage = ScyllaDB.deserialize(
+                    storage_config, self._cache_client, self._config.resources
+                )
+                self._nosql_storage.logging_handlers = self._logging_handlers
+            else:
+                self.logging.error(
+                    "The local deployment does not support "
+                    f"the NoSQL storage config type: {type(storage_config)}!"
+                )
+                raise RuntimeError("Cannot work with the provided NoSQL storage!")
+
+        return self._nosql_storage
