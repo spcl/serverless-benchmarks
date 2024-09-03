1f3cd9941aae63ef2cc39887d7ce66f8c3ae932b
mahlashrifi <mahla7997@gmail.com>
2024-09-03 20:18:52 +0330

    Solve dependency issues of function

--- benchmarks/600.microservice/610.teastore/611.authentication/functions/check_login/faas/LoggedInChecker.java
+++ benchmarks/600.microservice/610.teastore/611.authentication/functions/check_login/faas/LoggedInChecker.java
@@ -1,29 +1,31 @@
 package faas;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import util.Order;
 import util.SessionBlob;
 import util.ShaSecurityProvider;
-import jakarta.ws.rs.core.Response;
 
+//import jakarta.ws.rs.core.Response;
 
 public class LoggedInChecker {
     public static JsonObject main(JsonObject args) {
         Gson gson = new Gson();
         SessionBlob blob = gson.fromJson(args, SessionBlob.class);
 
-        //main code
-        blob.setUID(null);
-        blob.setSID(null);
-        blob.setOrder(new Order());
-        blob.getOrderItems().clear();
+        ShaSecurityProvider securityProvider = new ShaSecurityProvider();
+        SessionBlob validatedBlob = securityProvider.validate(blob);
 
-//        JsonObject jsonBlob = gson.toJsonTree(blob).getAsJsonObject();
-//        jsonBlob.addProperty("status", "logout successful");
+        JsonObject jsonResult = new JsonObject();
+        if (validatedBlob != null)
+            jsonResult.addProperty("Authorization-Status", "Authorized");
+        else
+            jsonResult.addProperty("Authorization-Status", "Unauthorized");
 
-        Response.status(Response.Status.OK).entity(new ShaSecurityProvider().validate(blob)).build();
-        JsonObject jsonBlob = gson.toJsonTree(blob).getAsJsonObject();
-        return jsonBlob;
+// Base version of output in TeaStore is commented:
+//        Response.status(Response.Status.OK).entity(new ShaSecurityProvider().validate(blob)).build();
 
+        return jsonResult;
     }
+
+
 }
+
