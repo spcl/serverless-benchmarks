a5efdb216bcd5f9260498a7b71050048abc034a9
Marcin Copik <mcopik@gmail.com>
2024-11-06 13:29:47 +0100

    [Feat] Support Python 3.10 on ARM

--- dockerfiles/python_installer.sh
+++ dockerfiles/python_installer.sh
@@ -2,12 +2,23 @@
 
 cd /mnt/function
 
+PLATFORM_ARG=""
+if [[ "${TARGET_ARCHITECTURE}" == "arm64" ]]; then
+  PLATFORM_ARG="--platform manylinux_2_17_aarch64 --only-binary=:all:"
+fi
+
 if [[ "${TARGET_ARCHITECTURE}" == "arm64" ]] && [[ -f "requirements.txt.arm.${PYTHON_VERSION}" ]]; then
-  pip3 -q install --platform manylinux_2_17_aarch64 --only-binary=:all: -r requirements.txt.arm.${PYTHON_VERSION} -t .python_packages/lib/site-packages
+
+  pip3 -q install ${PLATFORM_ARG} -r requirements.txt.arm.${PYTHON_VERSION} -t .python_packages/lib/site-packages
+
 elif [[ -f "requirements.txt.${PYTHON_VERSION}" ]]; then
-  pip3 -q install -r requirements.txt -r requirements.txt.${PYTHON_VERSION} -t .python_packages/lib/site-packages
+
+  pip3 -q install ${PLATFORM_ARG} -r requirements.txt.${PYTHON_VERSION} -t .python_packages/lib/site-packages
+
 else
-  pip3 -q install -r requirements.txt -t .python_packages/lib/site-packages
+
+  pip3 -q install ${PLATFORM_ARG} -r requirements.txt -t .python_packages/lib/site-packages
+
 fi
 
 if [[ -f "${SCRIPT_FILE}" ]]; then
