a14a7b0eb3f53eec6fd823f17499778fc0a34d8d
Larissa Schmid <larissa.schmid@kit.edu>
2025-02-12 09:12:53 +0100

    adapt input function of selfish-detour

--- benchmarks/600.workflows/640.selfish-detour/input.py
+++ benchmarks/600.workflows/640.selfish-detour/input.py
@@ -7,6 +7,6 @@ size_generators = {
 def buckets_count():
     return (0, 0)
 
-def generate_input(data_dir, size, input_buckets, output_buckets, upload_func):
+def generate_input(data_dir, size, benchmarks_bucket, input_buckets, output_buckets, upload_func, nosql_func):
     num_samples = size_generators[size]
     return { 'num_samples': num_samples }
