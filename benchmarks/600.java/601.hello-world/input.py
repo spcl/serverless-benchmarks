726e07a6c7fd73f04bcd8cdc14eff8ab40410b7f
mahlashrifi <mahla7997@gmail.com>
2025-05-11 01:05:22 +0330

    Add the nosql_func argument to the signature of generate_input

--- benchmarks/600.java/601.hello-world/input.py
+++ benchmarks/600.java/601.hello-world/input.py
@@ -1,5 +1,13 @@
 def buckets_count():
     return (0, 0)
 
-def generate_input(data_dir, size, benchmarks_bucket, input_paths, output_paths, upload_func):
+def generate_input(
+    data_dir,
+    size,
+    benchmarks_bucket,
+    input_paths,
+    output_paths,
+    upload_func,
+    nosql_func=None  
+):
     return { }
