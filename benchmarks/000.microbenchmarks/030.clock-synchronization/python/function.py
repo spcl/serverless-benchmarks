d6c422745a9d2d399e316b5e58232054a8f11b06
McLavish <Luca27ita@gmail.com>
2025-11-05 10:45:00 +0100

    benchmarks is now flake8/black compliant. pre-commit hooks also check this

--- benchmarks/000.microbenchmarks/030.clock-synchronization/python/function.py
+++ benchmarks/000.microbenchmarks/030.clock-synchronization/python/function.py
@@ -1,28 +1,27 @@
 import csv
-import json
 import os
 import socket
 from datetime import datetime
-from time import sleep
 
 from . import storage
 
+
 def handler(event):
 
-    request_id = event['request-id']
-    address = event['server-address']
-    port = event['server-port']
-    repetitions = event['repetitions']
-    output_bucket = event.get('bucket').get('bucket')
-    output_prefix = event.get('bucket').get('output')
+    request_id = event["request-id"]
+    address = event["server-address"]
+    port = event["server-port"]
+    repetitions = event["repetitions"]
+    output_bucket = event.get("bucket").get("bucket")
+    output_prefix = event.get("bucket").get("output")
     times = []
     print("Starting communication with {}:{}".format(address, port))
     i = 0
     socket.setdefaulttimeout(4)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(('', 0))
-    message = request_id.encode('utf-8')
+    server_socket.bind(("", 0))
+    message = request_id.encode("utf-8")
     adr = (address, port)
     consecutive_failures = 0
     measurements_not_smaller = 0
@@ -43,11 +42,13 @@ def handler(event):
         if i > 0:
             times.append([i, send_begin, recv_end])
         cur_time = recv_end - send_begin
-        print("Time {} Min Time {} NotSmaller {}".format(cur_time, cur_min, measurements_not_smaller))
+        print(
+            "Time {} Min Time {} NotSmaller {}".format(cur_time, cur_min, measurements_not_smaller)
+        )
         if cur_time > cur_min and cur_min > 0:
             measurements_not_smaller += 1
             if measurements_not_smaller == repetitions:
-                message = "stop".encode('utf-8')
+                message = "stop".encode("utf-8")
                 server_socket.sendto(message, adr)
                 break
         else:
@@ -57,18 +58,18 @@ def handler(event):
         consecutive_failures = 0
         server_socket.settimeout(4)
     server_socket.close()
-   
+
     if consecutive_failures != 5:
-        with open('/tmp/data.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(["id", "client_send", "client_rcv"]) 
+        with open("/tmp/data.csv", "w", newline="") as csvfile:
+            writer = csv.writer(csvfile, delimiter=",")
+            writer.writerow(["id", "client_send", "client_rcv"])
             for row in times:
                 writer.writerow(row)
-      
+
         client = storage.storage.get_instance()
-        filename = 'results-{}.csv'.format(request_id)
-        key = client.upload(output_bucket, os.path.join(output_prefix, filename), '/tmp/data.csv')
+        filename = "results-{}.csv".format(request_id)
+        key = client.upload(output_bucket, os.path.join(output_prefix, filename), "/tmp/data.csv")
     else:
         key = None
 
-    return { 'result': {'bucket-key': key, 'timestamp':  event['income-timestamp']} }
+    return {"result": {"bucket-key": key, "timestamp": event["income-timestamp"]}}
--- benchmarks/300.utilities/311.compression/python/function.py
+++ benchmarks/300.utilities/311.compression/python/function.py
@@ -1,12 +1,12 @@
 import datetime
-import io
 import os
 import shutil
 import uuid
-import zlib
 
 from . import storage
+
 client = storage.storage.get_instance()
+
 
 def parse_directory(directory):
 
@@ -16,13 +16,14 @@ def parse_directory(directory):
             size += os.path.getsize(os.path.join(root, file))
     return size
 
+
 def handler(event):
-  
-    bucket = event.get('bucket').get('bucket')
-    input_prefix = event.get('bucket').get('input')
-    output_prefix = event.get('bucket').get('output')
-    key = event.get('object').get('key')
-    download_path = '/tmp/{}-{}'.format(key, uuid.uuid4())
+
+    bucket = event.get("bucket").get("bucket")
+    input_prefix = event.get("bucket").get("input")
+    output_prefix = event.get("bucket").get("output")
+    key = event.get("object").get("key")
+    download_path = "/tmp/{}-{}".format(key, uuid.uuid4())
     os.makedirs(download_path)
 
     s3_download_begin = datetime.datetime.now()
@@ -31,29 +32,27 @@ def handler(event):
     size = parse_directory(download_path)
 
     compress_begin = datetime.datetime.now()
-    shutil.make_archive(os.path.join(download_path, key), 'zip', root_dir=download_path)
+    shutil.make_archive(os.path.join(download_path, key), "zip", root_dir=download_path)
     compress_end = datetime.datetime.now()
 
     s3_upload_begin = datetime.datetime.now()
-    archive_name = '{}.zip'.format(key)
+    archive_name = "{}.zip".format(key)
     archive_size = os.path.getsize(os.path.join(download_path, archive_name))
-    key_name = client.upload(bucket, os.path.join(output_prefix, archive_name), os.path.join(download_path, archive_name))
+    key_name = client.upload(
+        bucket, os.path.join(output_prefix, archive_name), os.path.join(download_path, archive_name)
+    )
     s3_upload_stop = datetime.datetime.now()
 
     download_time = (s3_download_stop - s3_download_begin) / datetime.timedelta(microseconds=1)
     upload_time = (s3_upload_stop - s3_upload_begin) / datetime.timedelta(microseconds=1)
     process_time = (compress_end - compress_begin) / datetime.timedelta(microseconds=1)
     return {
-            'result': {
-                'bucket': bucket,
-                'key': key_name
-            },
-            'measurement': {
-                'download_time': download_time,
-                'download_size': size,
-                'upload_time': upload_time,
-                'upload_size': archive_size,
-                'compute_time': process_time
-            }
-        }
-
+        "result": {"bucket": bucket, "key": key_name},
+        "measurement": {
+            "download_time": download_time,
+            "download_size": size,
+            "upload_time": upload_time,
+            "upload_size": archive_size,
+            "compute_time": process_time,
+        },
+    }
