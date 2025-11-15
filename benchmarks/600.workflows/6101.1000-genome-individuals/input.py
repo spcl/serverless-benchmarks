379dbf6c55319b4e5fc36201a0fcd65ad86012a6
McLavish <Luca27ita@gmail.com>
2025-11-15 17:03:53 +0100

    workflow changes for local execution

--- benchmarks/600.workflows/6101.1000-genome-individuals/input.py
+++ benchmarks/600.workflows/6101.1000-genome-individuals/input.py
@@ -4,21 +4,42 @@ import uuid
 import io
 
 size_generators = {
-    "test" : (1),
-    "small": (5),
-    "small-10": (10),
-    "small-20": (20),
-    "large": (10),
+    "test": 5,
+    "small": 5,
+    "small-10": 10,
+    "small-20": 20,
+    "large": 10,
 }
+
 
 def buckets_count():
     return (1, 1)
 
-def generate_input(data_dir, size, benchmarks_bucket, input_buckets, output_buckets, upload_func, nosql_func):
-    files = ["ALL.chr21.1250.vcf", "ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf", "columns.txt", "AFR", "ALL", "AMR", "EAS", "EUR", "GBR", "SAS"]
+
+def generate_input(
+    data_dir,
+    size,
+    benchmarks_bucket,
+    input_buckets,
+    output_buckets,
+    upload_func,
+    nosql_func,
+):
+    files = [
+        "ALL.chr21.1250.vcf",
+        "ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf",
+        "columns.txt",
+        "AFR",
+        "ALL",
+        "AMR",
+        "EAS",
+        "EUR",
+        "GBR",
+        "SAS",
+    ]
     for name in files:
         if name == "ALL.chr21.1250.vcf" or name == "columns.txt":
-        #if name != "ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf":
+            # if name != "ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf":
             path = os.path.join(data_dir, name)
             upload_func(0, name, path)
 
@@ -28,27 +49,25 @@ def generate_input(data_dir, size, benchmarks_bucket, input_buckets, output_buck
     start_bytes = 0
     with open(os.path.join(data_dir, files[0]), "r") as f:
         content = f.readlines()
-        range_per_job = 1250 / num_individuals_jobs
+        content = content[: min(len(content), 500)]
+        range_per_job = len(content) / num_individuals_jobs
         for i in range(0, num_individuals_jobs):
-            #actually split file; return it afterwards. see e.g. split.py in 660.map-reduce.
-            #regex = re.compile('(?!#)')
+            # actually split file; return it afterwards. see e.g. split.py in 660.map-reduce.
+            # regex = re.compile('(?!#)')
             start = i * range_per_job
             end = i * range_per_job + range_per_job
-            #print("start: ", start, "end: ", end, "range_per_job: ", range_per_job, "num_individuals_jobs: ", num_individuals_jobs)
-            #data = list(filter(regex.match, content[int(start):int(end)]))
-            data = content[int(start):int(end)]
-            #name with start and end lines is not needed as all individuals jobs can just read their entire file. 
+            # print("start: ", start, "end: ", end, "range_per_job: ", range_per_job, "num_individuals_jobs: ", num_individuals_jobs)
+            # data = list(filter(regex.match, content[int(start):int(end)]))
+            data = content[int(start) : int(end)]
+            # name with start and end lines is not needed as all individuals jobs can just read their entire file.
             name = str(uuid.uuid4())[:8]
-            
+
             upload_data = io.BytesIO()
             upload_data.writelines((val).encode("utf-8") for val in data)
             upload_data.seek(0)
             nbytes = upload_data.getbuffer().nbytes
 
-            output = {
-                "start_bytes": start_bytes,
-                "end_bytes": start_bytes + nbytes - 1
-            }
+            output = {"start_bytes": start_bytes, "end_bytes": start_bytes + nbytes - 1}
 
             blobs.append(output)
             start_bytes += nbytes
--- benchmarks/600.workflows/690.ml/python/train.py
+++ benchmarks/600.workflows/690.ml/python/train.py
@@ -9,9 +9,11 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
+
 def str_to_cls(cls_name):
-    #print(cls_name)
+    # print(cls_name)
     return globals()[cls_name]
+
 
 def load_dataset(benchmark_bucket, bucket, features, labels):
     dataset_dir = os.path.join("/tmp", str(uuid.uuid4()))
@@ -20,10 +22,9 @@ def load_dataset(benchmark_bucket, bucket, features, labels):
     features_path = os.path.join(dataset_dir, "features.npy")
     labels_path = os.path.join(dataset_dir, "labels.npy")
 
-
     client = storage.storage.get_instance()
-    client.download(benchmark_bucket, bucket + '/' + features, features_path)
-    client.download(benchmark_bucket, bucket + '/' + labels, labels_path)
+    client.download(benchmark_bucket, bucket + "/" + features, features_path)
+    client.download(benchmark_bucket, bucket + "/" + labels, labels_path)
 
     X = np.load(features_path)
     y = np.load(labels_path)
@@ -34,9 +35,7 @@ def load_dataset(benchmark_bucket, bucket, features, labels):
 def preprocess(X, y):
     X = StandardScaler().fit_transform(X)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.4, random_state=123
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=123)
 
     return X_train, X_test, y_train, y_test
 
@@ -55,7 +54,8 @@ def handler(schedule):
     y_key = schedule.pop("labels")
     bucket = schedule.pop("bucket")
     benchmark_bucket = schedule.pop("benchmark_bucket")
-    request_id = schedule.pop("request-id")
+    schedule.pop("request-id", None)
+    schedule.pop("request_id", None)
 
     clf = str_to_cls(name)(**schedule)
 
@@ -65,8 +65,4 @@ def handler(schedule):
     train(clf, X_train, y_train)
     score = val(clf, X_test, y_test)
 
-    return {
-        "name": name,
-        "score": score
-    }
-
+    return {"name": name, "score": score}
