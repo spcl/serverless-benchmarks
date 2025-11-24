e0b24f7b67f30ebbd917867e5e0cc8d51e5da915
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2025-11-24 18:23:51 +0100

    testing with actual data

--- benchmarks/600.workflows/6300.sda-workflow/python/filter.py
+++ benchmarks/600.workflows/6300.sda-workflow/python/filter.py
@@ -1,4 +1,6 @@
 import subprocess
 def handler(event):
-    subprocess.run(["SettlementDelineationFilter"])
-    return {}
+    input = "/data/Corvara_IT.tiff"
+    config = "/data/sda-workflow-local.json"
+    result = subprocess.run([f"SettlementDelineationFilter", "-i", input, "-c", config],capture_output=True,text=True)
+    return {"stdout": result.stdout, "stderr": result.stderr}
