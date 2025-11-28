88127085a4a58a96d837163e9e6b2074d1c840e5
ldzgch (MacOS) <ldzgch@gmail.com>
2025-11-28 15:34:06 +0100

    nosql wrapper - duarble object - may work

--- benchmarks/wrappers/cloudflare/python/handler.py
+++ benchmarks/wrappers/cloudflare/python/handler.py
@@ -2,7 +2,7 @@ import datetime, io, json, os, uuid, sys, ast
 import asyncio
 import importlib.util
 import traceback
-from workers import WorkerEntrypoint, Response
+from workers import WorkerEntrypoint, Response, DurableObject
 
 ## sys.path.append(os.path.join(os.path.dirname(__file__), '.python_packages/lib/site-packages'))
 
@@ -18,52 +18,10 @@ function/
 
 """
 
-
-def import_from_path(module_name, file_path):
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-    return module
-
-
-working_dir = os.path.dirname(__file__)
-
-class MakeAsync(ast.NodeTransformer):
-    def visit_FunctionDef(self, node):
-        if node.name != "handler":
-            return node
-        return ast.AsyncFunctionDef(
-            name=node.name,
-            args=node.args,
-            body=node.body,
-            decorator_list=node.decorator_list,
-            returns=node.returns,
-            type_params=node.type_params)
-
-class AddAwait(ast.NodeTransformer):
-    to_find = ["upload_stream", "download_stream", "upload", "download", "download_directory"]
-    
-    def visit_Call(self, node):
-        if isinstance(node.func, ast.Attribute) and node.func.attr in self.to_find:
-            #print(ast.dump(node.func, indent=2))
-            return ast.Await(value=node)
+class KVApiObject(DurableObject):
+    def __getattr__(self, name):
+        return getattr(self.ctx.storage, name)
         
-        return node
-        
-def make_benchmark_func():
-    with open(working_dir +"/function/function.py") as f:
-        module = ast.parse(f.read())
-    module = ast.fix_missing_locations(MakeAsync().visit(module))
-    module = ast.fix_missing_locations(AddAwait().visit(module))
-    new_source = ast.unparse(module)
-    ##print("new_source:")
-    ##print(new_source)
-    ##print()
-    with open("/tmp/function.py", "w") as wf:
-        wf.write(new_source)
-        
-
 class Default(WorkerEntrypoint):
     async def fetch(self, request, env):
         try:
@@ -93,8 +51,6 @@ class Default(WorkerEntrypoint):
                     event[param[0]] = param[1]
                 except IndexError:
                     event[param[0]] = None
-
-
 
 
 
@@ -152,3 +108,52 @@ class Default(WorkerEntrypoint):
                 'environ_container_id': "no_id",
                 'request_id': "0"
             }))
+
+
+### ---------- old -------
+
+def import_from_path(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
+
+
+working_dir = os.path.dirname(__file__)
+
+class MakeAsync(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        if node.name != "handler":
+            return node
+        return ast.AsyncFunctionDef(
+            name=node.name,
+            args=node.args,
+            body=node.body,
+            decorator_list=node.decorator_list,
+            returns=node.returns,
+            type_params=node.type_params)
+
+class AddAwait(ast.NodeTransformer):
+    to_find = ["upload_stream", "download_stream", "upload", "download", "download_directory"]
+    
+    def visit_Call(self, node):
+        if isinstance(node.func, ast.Attribute) and node.func.attr in self.to_find:
+            #print(ast.dump(node.func, indent=2))
+            return ast.Await(value=node)
+        
+        return node
+        
+def make_benchmark_func():
+    with open(working_dir +"/function/function.py") as f:
+        module = ast.parse(f.read())
+    module = ast.fix_missing_locations(MakeAsync().visit(module))
+    module = ast.fix_missing_locations(AddAwait().visit(module))
+    new_source = ast.unparse(module)
+    ##print("new_source:")
+    ##print(new_source)
+    ##print()
+    with open("/tmp/function.py", "w") as wf:
+        wf.write(new_source)
+        
+
--- benchmarks/wrappers/cloudflare/python/nosql.py
+++ benchmarks/wrappers/cloudflare/python/nosql.py
@@ -1,9 +1,111 @@
 from typing import List, Optional, Tuple
 import json
+import pickle
 from pyodide.ffi import to_js, run_sync
-from workers import WorkerEntrypoint
+from workers import WorkerEntrypoint, DurableObject
 
-class nosql:
+
+class nosql_do:
+    instance: Optional["nosql"] = None
+    DO_BINDING_NAME = "DURABLE_STORE"
+
+    @staticmethod
+    def init_instance(entry: WorkerEntrypoint):
+        nosql.instance = nosql()
+        nosql.instance.binding = getattr(entry.env, nosql_do.DO_BINDING_NAME)
+
+    
+    def get_table(self, table_name):
+        kvapiobj = self.binding.getByName(table_name)
+        return kvapiobj
+
+    def key_maker(self, key1, key2):
+            return f"({key1[0]},{str(key1[1])})+({key2[0]},{key2[1]})"
+    
+    def key_maker_partial(self, key1, key2):
+        return f"({key1[0]},{str(key1[1])})+({key2[0]}"
+
+## these data conversion funcs should not be necessary. i couldn't get pyodide to clone the data otherwise
+    def data_pre(self, data):
+        return pickle.dumps(data, 0).decode("ascii")
+
+    def data_post(self, data):
+        return pickle.loads(bytes(data, "ascii"))
+    
+    def insert(
+        self,
+        table_name: str,
+        primary_key: Tuple[str, str],
+        secondary_key: Tuple[str, str],
+        data: dict,
+    ):
+        send_data = self.data_pre(data)
+        k=self.key_maker(primary_key, secondary_key)
+        put_res = run_sync(self.get_table(table_name).put(k, send_data))
+        return
+
+    ## does this really need different behaviour from insert?
+    def update(
+        self,
+        table_name: str,
+        primary_key: Tuple[str, str],
+        secondary_key: Tuple[str, str],
+        data: dict,
+    ):
+        self.insert(table_name, primary_key, secondary_key, data)
+        return 
+
+
+    def get(
+        self, table_name: str, primary_key: Tuple[str, str], secondary_key: Tuple[str, str]
+    ) -> Optional[dict]:
+        k=self.key_maker(primary_key, secondary_key)
+        get_res = run_sync(self.get_table(table_name).get(k))
+        ## print(get_res)
+        return self.data_post(get_res)
+
+    """
+        This query must involve partition key - it does not scan across partitions.
+    """
+
+    def query(
+        self, table_name: str, primary_key: Tuple[str, str], secondary_key_name: str
+    ) -> List[dict]:
+
+        prefix_key = self.key_maker_partial(primary_key, (secondary_key_name,))
+        list_res = run_sync(self.get_table(table_name).list())
+        
+        keys = []
+        for key in list_res:
+            if key.startswith(prefix_key):
+                print(key)
+                keys.append(key)
+        ##print("keys", keys)
+        assert len(keys) <= 100
+
+
+        # todo: please use bulk sometime (it didn't work when i tried it)
+        res = []
+        for key in keys:
+            
+            get_res = run_sync(self.get_table(table_name).get(key))
+            ## print(get_res)
+            res.append(self.data_post(get_res))
+        return res
+
+    def delete(self, table_name: str, primary_key: Tuple[str, str], secondary_key: Tuple[str, str]):
+        run_sync(self.get_table(table_name).delete(self.key_maker(primary_key, secondary_key)))
+        return
+
+    @staticmethod
+    def get_instance():
+        if nosql_do.instance is None:
+            nosql_do.instance = nosql_do()
+        return nosql_do.instance
+
+### ------------------------------
+
+class nosql_kv:
 
     instance: Optional["nosql"] = None
 
@@ -96,3 +198,8 @@ class nosql:
         if nosql.instance is None:
             nosql.instance = nosql()
         return nosql.instance
+
+
+
+
+nosql = nosql_do
