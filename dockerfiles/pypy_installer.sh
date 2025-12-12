47f6a8f363c313196fe97c9d95515b9ad4e3be39
toooadi <toooadi0@gmail.com>
2025-12-12 12:34:59 +0100

    Adapt pypy installer

--- dockerfiles/pypy_installer.sh
+++ dockerfiles/pypy_installer.sh
@@ -2,19 +2,6 @@
 
 cd /mnt/function
 
-#TODO: If the base image OS is not centOS based, change to apt
-yum install -y tar bzip2 gzip
-
-#TODO: make version configurable
-curl -L -o pypy.tar.bz2 https://downloads.python.org/pypy/pypy3.11-v7.3.20-linux64.tar.bz2
-tar -xjf pypy.tar.bz2 
-mv pypy3.11-v7.3.20-linux64 /opt/pypy 
-rm pypy.tar.bz2
-chmod -R +x /opt/pypy/bin
-export PATH=/opt/pypy/bin:$PATH
-python -m ensurepip
-python -mpip install -U pip wheel
-
 #Probably remove this conditional, might break pypy builds, might lead to installation of CPython libraries
 PLATFORM_ARG=""
 if [[ "${TARGET_ARCHITECTURE}" == "arm64" ]]; then
