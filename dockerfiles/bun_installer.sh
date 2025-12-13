e29be382139aad8c4dfdf9a0f3e6ce4985ed0d0c
Markbeep <mrkswrn@gmail.com>
2025-12-13 10:02:13 +0100

    build for correct architecture

--- dockerfiles/bun_installer.sh
+++ dockerfiles/bun_installer.sh
@@ -4,13 +4,16 @@ cd /mnt/function
 
 if [ "${TARGET_ARCHITECTURE}" == "arm64" ]; then
         bun install --arch=arm64
+        COMPILE_TARGET=bun-linux-arm64
 elif [ "${TARGET_ARCHITECTURE}" = "x64" ]; then
         bun install --arch=x64
+        COMPILE_TARGET=bun-linux-x64
 else
         echo "Unsupported architecture: $TARGET_ARCHITECTURE"
         exit 1
 fi
 
 
-bun build --compile --minify --sourcemap runtime.js --outfile bootstrap
+bun build --compile --minify --target $COMPILE_TARGET --sourcemap runtime.js --outfile bootstrap
+# For AWS we need to compile the bun/js files into a bootstrap binary
 rm -r bun.lock node_modules function.js handler.js package.json runtime.js storage.js
