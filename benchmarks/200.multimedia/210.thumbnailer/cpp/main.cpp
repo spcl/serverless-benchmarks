eb66f613296f3c26311e0d4513f909bb5cacbf7d
Marcin Copik <mcopik@gmail.com>
2026-02-27 14:08:11 +0100

    [benchmarks] Implement second C++ version of 210.thumbnailer
    This one uses libjpeg-turbo for faster loading

--- benchmarks/200.multimedia/210.thumbnailer/cpp/main.cpp
+++ benchmarks/200.multimedia/210.thumbnailer/cpp/main.cpp
@@ -2,10 +2,16 @@
 #include <aws/core/Aws.h>
 #include <aws/core/utils/json/JsonSerializer.h>
 
-#include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <turbojpeg.h>
+
+#include <string>
 #include <iostream>
 #include <vector>
 
@@ -13,13 +19,20 @@
 #include "storage.hpp"
 #include "utils.hpp"
 
+bool opencv_initialized = false;
+
+
 Aws::Utils::Json::JsonValue function(Aws::Utils::Json::JsonView request)
 {
-  sebs::Storage client_ = sebs::Storage::get_client();
+
+  auto process_start = timeSinceEpochMicrosec();
+  static sebs::Storage client_ = sebs::Storage::get_client();
+  auto process_end = timeSinceEpochMicrosec();
+
+  std::cerr << (process_end - process_start) << " us taken to create storage client" << std::endl;
 
   auto bucket_obj = request.GetObject("bucket");
-  if (!bucket_obj.IsObject())
-  {
+  if (!bucket_obj.IsObject()) {
     Aws::Utils::Json::JsonValue error;
     error.WithString("error", "Bucket object is not valid.");
     return error;
@@ -32,6 +45,8 @@ Aws::Utils::Json::JsonValue function(Aws::Utils::Json::JsonView request)
   auto width = request.GetObject("object").GetInteger("width");
   auto height = request.GetObject("object").GetInteger("height");
 
+  //std::cerr << cv::getBuildInformation() << std::endl;
+
   std::string body_str;
   uint64_t download_time;
   {
@@ -40,8 +55,7 @@ Aws::Utils::Json::JsonValue function(Aws::Utils::Json::JsonView request)
     body_str = std::get<0>(ans);
     download_time = std::get<1>(ans);
 
-    if (body_str.empty())
-    {
+    if (body_str.empty()) {
       Aws::Utils::Json::JsonValue error;
       error.WithString("error", "Failed to download object from S3: " + input_key);
       return error;
@@ -49,55 +63,56 @@ Aws::Utils::Json::JsonValue function(Aws::Utils::Json::JsonView request)
   }
 
   std::vector<char> vectordata(body_str.begin(), body_str.end());
-  cv::Mat image = imdecode(cv::Mat(vectordata), 1);
 
-  // Apply the thumbnailer function and measure the computing time
-  cv::Mat image2;
   uint64_t computing_time;
+  cv::Mat out_image;
   {
     auto start_time = timeSinceEpochMicrosec();
-    thumbnailer(image, width, height, image2);
+    thumbnailer(vectordata, width, height, out_image);
+    //thumbnailer_fast(vectordata, width, height, out_image);
     computing_time = timeSinceEpochMicrosec() - start_time;
   }
 
+  auto encode_start = timeSinceEpochMicrosec();
   std::vector<unsigned char> out_buffer;
-  cv::imencode(".jpg", image2, out_buffer);
+  cv::imencode(".jpg", out_image, out_buffer);
+  auto encode_end = timeSinceEpochMicrosec();
+  std::cerr << (encode_end - encode_start) << " us taken for encode" << std::endl;
 
   // Create a unique key name for the output image
   std::string key_name;
+  std::string key_name2;
   {
     std::string output_key = output_key_prefix + "/" + image_name;
     std::string name, extension;
-    if (output_key.find_last_of('.') != std::string::npos)
-    {
+    if (output_key.find_last_of('.') != std::string::npos) {
       name = output_key.substr(0, output_key.find_last_of('.'));
       extension = output_key.substr(output_key.find_last_of('.'));
-    }
-    else
-    {
+    } else {
       name = output_key;
       extension = "";
     }
-    key_name = name + "."
-        + boost::uuids::to_string(boost::uuids::random_generator()())
-        + extension;
+    key_name = name + "." +
+               boost::uuids::to_string(boost::uuids::random_generator()()) +
+               extension;
+    key_name2 = name + ".tmp." +
+                boost::uuids::to_string(boost::uuids::random_generator()()) +
+                extension;
   }
 
   // Upload the resulting image to S3
-  // If the upload fails, return an error
   Aws::String upload_data(out_buffer.begin(), out_buffer.end());
 
   uint64_t upload_time = client_.upload_random_file(
-      bucket_name, key_name, true,
-      reinterpret_cast<char *>(out_buffer.data()), out_buffer.size());
+    bucket_name, key_name, true, reinterpret_cast<char *>(out_buffer.data()),
+    out_buffer.size()
+  );
 
-  if (upload_time == 0)
-  {
+  if (upload_time == 0) {
     Aws::Utils::Json::JsonValue error;
     error.WithString("error", "Failed to upload object to S3: " + key_name);
     return error;
   }
-
 
   Aws::Utils::Json::JsonValue val;
   Aws::Utils::Json::JsonValue result;
@@ -112,6 +127,6 @@ Aws::Utils::Json::JsonValue function(Aws::Utils::Json::JsonView request)
   measurements.WithInteger("compute_time", computing_time);
   measurements.WithInteger("download_size", vectordata.size());
   measurements.WithInteger("upload_size", out_buffer.size());
-  val.WithObject("measurements", measurements);
+  val.WithObject("measurement", measurements);
   return val;
 }
